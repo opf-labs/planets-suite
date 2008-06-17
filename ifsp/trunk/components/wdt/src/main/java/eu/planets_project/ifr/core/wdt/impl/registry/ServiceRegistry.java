a4deb3e058c9a7a2a27744d582e9e0feecc63142
Rainer Schmidt <rainer.schmidt@ait.ac.at>
2008-06-17 15:08:17 +0000

    integrated migratebinaryoneservice

--- ifsp/trunk/components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/impl/registry/ServiceRegistry.java
+++ ifsp/trunk/components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/impl/registry/ServiceRegistry.java
@@ -17,6 +17,8 @@ import org.apache.commons.logging.Log;
 
 import eu.planets_project.ifr.core.common.logging.PlanetsLogger;
 import eu.planets_project.ifr.core.wdt.impl.registry.Service;
+import eu.planets_project.ifr.core.common.registry.RegistryClient;
+
 import eu.planets_project.ifr.core.wdt.common.services.serviceRegistry.ServiceRegistryManager_Service;
 import eu.planets_project.ifr.core.wdt.common.services.serviceRegistry.ServiceRegistryManager;
 import eu.planets_project.ifr.core.wdt.common.services.serviceRegistry.PsService;
@@ -42,14 +44,20 @@ public class ServiceRegistry
 
 	private Log logger = PlanetsLogger.getLogger(this.getClass(), "resources/log/wdt-log4j.xml");	
 	private ServiceRegistryManager registry = null;
+	private URL registryUrl = null;
 	private List<Service> dummyServices = null;
+	private RegistryClient regWriter = null;
+
 
 	public ServiceRegistry() {
 		//locate service registry
 		try {
-			//ServiceRegistryManager_Service locator = new ServiceRegistryManager_Service(new URL("http://planetarium.hki.uni-koeln.de:8080/registry-ifr-registry-ejb/ServiceRegistryManager?wsdl"), new QName("http://planets-project.eu/ifr/core/registry", "ServiceRegistryManager"));
-			ServiceRegistryManager_Service locator = new ServiceRegistryManager_Service(new URL("http://localhost:8080/registry-ifr-registry-ejb/ServiceRegistryManager?wsdl"), new QName("http://planets-project.eu/ifr/core/registry", "ServiceRegistryManager"));
+			registryUrl = new URL("http://localhost:8080/registry-ifr-registry-ejb/ServiceRegistryManager?wsdl");
+			//registryUrl = new URL("http://planetarium.hki.uni-koeln.de:8080/registry-ifr-registry-ejb/ServiceRegistryManager?wsdl";			
+			ServiceRegistryManager_Service locator = new ServiceRegistryManager_Service(registryUrl, new QName("http://planets-project.eu/ifr/core/registry", "ServiceRegistryManager"));
 			registry = locator.getServiceRegistryManagerPort();
+			regWriter = new RegistryClient(registryUrl);			
+			
 		} catch(Exception e) {
 			logger.error("Error testing registry: ", e);
 		}			
@@ -147,10 +155,8 @@ public class ServiceRegistry
 			String taxonomyId = schema.getId();
 			logger.error("Schema ID: "+taxonomyId);
 		
-			//initialize cids
 			//planetsservice[0], characterisation[1], emulation[2], migration[3], identification[4], 
-			//validation[5], metadataextraction[6], composite[7], comparison[8]
-			
+			//validation[5], metadataextraction[6], composite[7], comparison[8]			
 			catIds = new String[categories.size()];
 			
 			//find Taxonomies
@@ -174,15 +180,74 @@ public class ServiceRegistry
 				orgId = obj.getKey();
 				logger.error("found org key: "+orgId);
 			}
-
-			//Dummy Monitor
-			//PsService service = new PsService();			
-			//serviceId = registerService(service, "Test1@localhost", "Test1.Desc", org);
-			//service.setKey(serviceId);
-			//registerBinding(service, "Binding.Name", "Bindin.Dsc", "http://dme023:8080/wdt-ifr-wdt-ejb/MonitorImpl?wsdl");
-			//PsRegistryMessage rMsg = registry.addClassificationTo("provider","provider",serviceId,catIds[0]);
+		
+			serviceId = regWriter.registerService("MagicJpg2Tiff@localhost", "service_dsc");
+			regWriter.registerBinding(serviceId, "local_binding", "binding_desc", "http://localhost:8080/ifr-jmagickconverter-ejb/JpgToTiffConverter?wsdl");
+			regWriter.addCategory(serviceId, regWriter.categoryIds[3]);
 			
-			PsService service = new PsService();			
+			serviceId = regWriter.registerService("MagicIdentification@localhost", "service_dsc");
+			regWriter.registerBinding(serviceId, "local_binding", "binding_desc", "http://localhost:8080/ifr-jmagickconverter-ejb/ImageIdentificationService?wsdl");
+			regWriter.addCategory(serviceId, regWriter.categoryIds[1]);			
+			
+			serviceId = regWriter.registerService("XenaDoc2ODF@localhost", "service_dsc");
+			regWriter.registerBinding(serviceId, "local_binding", "binding_desc", "http://localhost:8080/pserv-pa-xena/DocToODFXena?wsdl");
+			regWriter.addCategory(serviceId, regWriter.categoryIds[3]);			
+			
+			serviceId = regWriter.registerService("Droid@localhost", "service_dsc");
+			regWriter.registerBinding(serviceId, "local_binding", "binding_desc", "http://localhost:8080/pserv-pc-droid/Droid?wsdl");
+			regWriter.addCategory(serviceId, regWriter.categoryIds[1]);	
+			
+			serviceId = regWriter.registerService("XenaODF2PDF@localhost", "service_dsc");
+			regWriter.registerBinding(serviceId, "local_binding", "binding_desc", "jboss.ws:context=pserv-pa-xena,endpoint=ODFToPDFXena");
+			regWriter.addCategory(serviceId, regWriter.categoryIds[3]);									
+
+		} catch(Exception e) {
+			logger.error("Error testing registry: ", e);
+		}
+	}
+	
+	public RegistryClient getRegistryWriter() {
+		return this.regWriter;
+	}
+}	
+	
+
+/*
+	public String registerService(PsService service, String name, String dsc, PsOrganization org) throws Exception {
+		String serviceId = null;
+		
+		service.setName(name);
+		service.setDescription(dsc);
+		service.setOrganization(org);
+			
+		PsRegistryMessage rMsg = registry.saveService("provider", "provider", service);
+		
+		//String msg = rMsg.getMessage();
+		//logger.error("saved service message: "+msg);
+		List<String> operands = rMsg.getOperands();
+		//find ServiceId
+		for(int y = 0; y < operands.size(); y++) {
+			serviceId = operands.get(y);
+			//logger.error("saved service key: "+serviceId);
+		}
+		return serviceId;
+	}
+			
+	public void registerBinding(PsService service, String name, String dsc, String url) throws Exception {
+		//create Binding
+		PsBinding binding = new PsBinding();
+		binding.setService(service);
+		binding.setName(name);
+		binding.setDescription(dsc);
+		binding.setAccessuri(url);
+		//logger.error("register binding for servicee key: "+binding.getService().getKey());		
+		PsRegistryMessage rMsg = registry.saveServiceBinding("provider","provider",binding);
+	}
+
+*/	
+	
+	/*
+				PsService service = new PsService();			
 			serviceId = registerService(service, "SimpleCharacterisation@localhost", "Characterizes by Mime Types", org);
 			logger.debug("registering service: "+ "name: SimpleCharacterisation@rainer.arc.at" + "dsc: Characterizes by Mime Types" + "org:"+org.getKey());
 			logger.debug("received service id: "+ serviceId);
@@ -210,9 +275,9 @@ public class ServiceRegistry
 			registerBinding(service, "local.binding3", "binding.dsc", "http://localhost:8080/ifr-jmagickconverter-ejb/JpgToTiffConverter?wsdl");
 			logger.debug("registering binding for service: "+service.getKey()+"name: local.binding3" + "dsc: binding.dsc" + "uri: http://localhost:8080/ifr-jmagickconverter-ejb/JpgToTiffConverter?wsdl");			
 			rMsg = registry.addClassificationTo("provider","provider",serviceId,catIds[3]);			
-			logger.debug("adding classification to service: "+serviceId+" category: "+catIds[3]);			
+			logger.debug("adding classification to service: "+serviceId+" category: "+catIds[3]);
 			
-			//service = new PsService();			
+				//service = new PsService();			
 			//serviceId = registerService(service, "Doc2OpenXML@localhost", "Converts doc format to docx", org);
 			//service.setKey(serviceId);
 			//registerBinding(service, "local.binding", "binding.dsc", "http://localhost:8080/ifr-openXML-ejb/OpenXMLMigration?wsdl");
@@ -230,52 +295,9 @@ public class ServiceRegistry
 			//registerBinding(service, "local.binding", "binding.dsc", "http://localhost:8080/ReportGenerationService/ReportGenerationService?wsdl");
 			//rMsg = registry.addClassificationTo("provider","provider",serviceId,catIds[3]);			
 
-		} catch(Exception e) {
-			logger.error("Error testing registry: ", e);
-		}
-	}
-	
-	
-	/**
-	 * Method to programmatically add a services to the registry
-	 */
-	public String registerService(PsService service, String name, String dsc, PsOrganization org) throws Exception {
-		String serviceId = null;
-		
-		service.setName(name);
-		service.setDescription(dsc);
-		service.setOrganization(org);
-			
-		PsRegistryMessage rMsg = registry.saveService("provider", "provider", service);
-		
-		//String msg = rMsg.getMessage();
-		//logger.error("saved service message: "+msg);
-		List<String> operands = rMsg.getOperands();
-		//find ServiceId
-		for(int y = 0; y < operands.size(); y++) {
-			serviceId = operands.get(y);
-			//logger.error("saved service key: "+serviceId);
-		}
-		return serviceId;
-	}
-			
-	/**
-	 * Method to programmatically add a binding to the registry
-	 */
-	public void registerBinding(PsService service, String name, String dsc, String url) throws Exception {
-		//create Binding
-		PsBinding binding = new PsBinding();
-		binding.setService(service);
-		binding.setName(name);
-		binding.setDescription(dsc);
-		binding.setAccessuri(url);
-		//logger.error("register binding for servicee key: "+binding.getService().getKey());		
-		PsRegistryMessage rMsg = registry.saveServiceBinding("provider","provider",binding);
-	}
 
-	/**
-	* deprecated method to fill dummy services list
 	*/
+	
 	
 	/* public void createDummyServices() {
 		
@@ -327,4 +349,4 @@ public class ServiceRegistry
 				   					   repGeneratorQname) );    				   					  
 	} */
 	
-}
+
