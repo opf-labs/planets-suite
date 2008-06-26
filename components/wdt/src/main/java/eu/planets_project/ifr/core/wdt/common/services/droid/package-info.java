3bbe10ca36b3ac1230244cc56c8c51dbeb7836b5
Rainer Schmidt <rainer.schmidt@ait.ac.at>
2008-06-26 14:40:44 +0000

    deleting not required web service stubs

--- components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/common/services/droid/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://planets-project.eu/services")
-package eu.planets_project.ifr.core.wdt.common.services.droid;
--- components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/common/services/jpeg2tiff/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://planets-project.eu/ifr/migration")
-package eu.planets_project.ifr.core.wdt.common.services.jpeg2tiff;
