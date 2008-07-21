d33d09a49a5326b749c66713f50710175963d835
Fabian Steeg <fsteeg@gmail.com>
2008-07-21 09:57:50 +0000

    Added sample files to the tests of the metadata extractor service.

--- PC/metadata/src/test/java/eu/planets_project/ifr/core/services/characterisation/metadata/MatadataExtractorTests.java
+++ PC/metadata/src/test/java/eu/planets_project/ifr/core/services/characterisation/metadata/MatadataExtractorTests.java
@@ -24,6 +24,39 @@ import eu.planets_project.ifr.core.services.characterisation.metadata.impl.Metad
  */
 public class MatadataExtractorTests {
 
+	public static final String SAMPLES = "PC/metadata/src/resources/samples/";
+
+	public enum MetadataType {
+		/** Some types work just fine and give a full result: */
+		BMP("bmp/test1.bmp", "image/bmp"),
+		GIF("gif/AA_Banner.gif", "image/gif"),
+		JPEG("jpeg/AA_Banner.jpg", "image/jpeg"),
+		TIFF("tiff/AA_Banner.tif", "image/tiff"),
+		PDF("pdf/AA.pdf", "application/pdf"),
+		WAV("wav/comet.wav", "wave"),
+		HTML("html/sample.html", "text/html"),
+		/** The OO adapter throws an exception but still works: */
+		OPEN_OFFICE1("oo1/planets.sxw", "application/open-office-1.x"),
+		/**
+		 * And some are characterized as unknown although they should be
+		 * supported (according to http://meta-extractor.sourceforge.net):
+		 */
+		WORD_PERFECT("wordperfect/sample.wpd", "file/unknown"),
+		WORD6("word6/planets.doc", "file/unknown"),
+		WORKS("works/sample.wps", "file/unknown"),
+		EXCEL("excel/Travel.xls", "file/unknown"),
+		POWER_POINT("pp/planets.ppt", "file/unknown"),
+		MP3("mp3/Arkansas.mp3", "file/unknown"),
+		XML("xml/sample.xml", "file/unknown");
+		private String mime;
+		private String location;
+
+		private MetadataType(String location, String type) {
+			this.location = location;
+			this.mime = type;
+		}
+	}
+
 	/**
 	 * Tests MetadataExtractor characterization using a local MetadataExtractor
 	 * instance
@@ -65,13 +98,16 @@ public class MatadataExtractorTests {
 	 */
 	private void test(BasicCharacteriseOneBinary characterise)
 			throws PlanetsException {
-		File file = new File("PC/metadata/src/resources/samples/pdf/AA.pdf");
-		byte[] binary = ByteArrayHelper.read(file);
-		String result = characterise.basicCharacteriseOneBinary(binary);
-		System.out.println("Characterised " + file.getAbsolutePath() + " as: "
-				+ result);
-		assertTrue("Result does not contain the correct mime type;", result
-				.toLowerCase().contains("application/pdf"));
-		/* TODO test all supported file types, as in JHOVE service */
+		/* Test all file types supported by the tool */
+		for (MetadataType type : MetadataType.values()) {
+			System.out.println("Testing characterisation of " + type);
+			File file = new File(SAMPLES + type.location);
+			byte[] binary = ByteArrayHelper.read(file);
+			String result = characterise.basicCharacteriseOneBinary(binary);
+			System.out.println("Characterised " + file.getAbsolutePath()
+					+ " as: " + result);
+			assertTrue("Result does not contain the correct mime type: "
+					+ type.mime, result.toLowerCase().contains(type.mime));
+		}
 	}
 }
