da625b944041f2b9b015c31aab3f97844e38dfd7
Andrew Jackson <Andrew.Jackson@bl.uk>
2008-10-30 10:36:50 +0000

    Modified the ValidateResult to distinguish between invalid, well-formed and valid.

--- IF/common/src/java/eu/planets_project/services/validate/ValidateResult.java
+++ IF/common/src/java/eu/planets_project/services/validate/ValidateResult.java
@@ -17,13 +17,56 @@ import eu.planets_project.services.datatypes.ServiceReport;
 @XmlAccessorType(value = XmlAccessType.FIELD)
 public class ValidateResult {
 
-    boolean valid;
+    /**
+     * A description of the validity of the object.
+     */
+    enum Validity { 
+        /** 'Invalid' means that the digital object does not appear to be in the proposed format. */
+        INVALID, 
+        
+        /** 'Well Formed' means that the digital object appears to be structurally sound, but the content could not be fully semantically checked or internally verified. */
+        WELL_FORMED, 
+
+        /** 'Valid' means that the digital object is not only well formed, but also appears to be semantically valid and self-consistent. */
+        VALID 
+    };
+    private Validity validity;
     
-    ServiceReport resport;
+    /**
+     * 
+     */
+    private ServiceReport report;
 
     /**
      * No-args constructor required by JAXB
      */
-    public ValidateResult() { }
+    private ValidateResult() {}
+    
+    /**
+     * Creates a new validation result, containing a description of the Validity and a Service Report.
+     * 
+     * @param validity The Validity of the digital object, against the proposed format.
+     * @param report The service report.
+     */
+    public ValidateResult(Validity validity, ServiceReport report ) {
+        this.validity = validity;
+        this.report = report;
+    }
+
+    /**
+     * @return the validity
+     */
+    public Validity getValidity() {
+        return validity;
+    }
+
+    /**
+     * @return the report
+     */
+    public ServiceReport getReport() {
+        return report;
+    }
+    
+    
 
 }
