a329d1a78cd3412b6bb4bcd391fdad6ac5cddf9c
Andrew Jackson <Andrew.Jackson@bl.uk>
2008-11-14 11:20:34 +0000

    Switched these services to use the new ServiceDescription.Builder.

--- PA/jmagickconverter/src/test/java/eu/planets_project/ifr/core/services/migration/jmagickconverter/impl/ImageMagickMigrationsTest.java
+++ PA/jmagickconverter/src/test/java/eu/planets_project/ifr/core/services/migration/jmagickconverter/impl/ImageMagickMigrationsTest.java
@@ -56,7 +56,7 @@ public class ImageMagickMigrationsTest {
     @Test
     public void testDescribe() {
         ServiceDescription desc = imageMagick.describe();
-        System.out.println("Recieved service description: " + desc.toXml(true));
+        System.out.println("Recieved service description: " + desc.toXmlFormatted());
         assertTrue("The ServiceDescription should not be NULL.", desc != null );
     }
     
