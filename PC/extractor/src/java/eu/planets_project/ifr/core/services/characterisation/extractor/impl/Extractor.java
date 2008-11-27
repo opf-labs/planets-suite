fd363ca638ff3e84a6be34d8dea37ebc4bda6b27
Peter Melms <peter.melms@uni-koeln.de>
2008-11-27 10:18:32 +0000

    - deleted Java 6 @override annotation to conform to Java 1.5

--- PC/extractor/src/java/eu/planets_project/ifr/core/services/characterisation/extractor/impl/Extractor.java
+++ PC/extractor/src/java/eu/planets_project/ifr/core/services/characterisation/extractor/impl/Extractor.java
@@ -12,20 +12,17 @@ import eu.planets_project.services.datatypes.ServiceDescription;
 
 public class Extractor implements Characterise {
 
-	@Override
 	public CharacteriseResult characterise(DigitalObject digitalObject,
 			String optionalFormatXCEL, Parameters parameters) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ServiceDescription describe() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<Property> listProperties(URI formatURI) {
 		// TODO Auto-generated method stub
 		return null;
