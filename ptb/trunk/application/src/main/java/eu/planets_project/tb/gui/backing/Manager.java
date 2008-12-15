a3d80014f310f4bb29844bc740de3fd22990bc18
Andrew Jackson <Andrew.Jackson@bl.uk>
2008-12-15 15:13:48 +0000

    Added a page so that the email configuration can be tested.

--- ptb/trunk/application/src/main/java/eu/planets_project/tb/gui/backing/Manager.java
+++ ptb/trunk/application/src/main/java/eu/planets_project/tb/gui/backing/Manager.java
@@ -1,6 +1,8 @@
 package eu.planets_project.tb.gui.backing;
 
 import javax.faces.context.FacesContext;
+
+import eu.planets_project.ifr.core.common.mail.PlanetsMailMessage;
 import eu.planets_project.tb.gui.backing.admin.RegisterTBServices;
 import eu.planets_project.tb.gui.backing.admin.ManagerTBServices;
 import eu.planets_project.tb.gui.backing.admin.wsclient.faces.WSClientBean;
@@ -8,8 +10,6 @@ import eu.planets_project.tb.gui.backing.exp.AutoBMGoalEvalUserConfigBean;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.net.URI;
 
 /**
  * @author Andrew Lindley, ARC
@@ -111,6 +111,91 @@ public class Manager {
 		FacesContext ctx = FacesContext.getCurrentInstance();
 		ctx.getExternalContext().getSessionMap().put("AutoEvalSerUserConfigBean", evalConfBean);
     }
+    
+    /* --- Test user email sending... */
+    
+    private String emailTestAddress = "???";
+    private String emailTestSubject = "This is a test email.";
+    private String emailTestMessage = "This is a test email, send from the Testbed.";
+    private String testEmailResult = "";
 
+    /**
+     * @return the emailTestAddress
+     */
+    public String getEmailTestAddress() {
+        return emailTestAddress;
+    }
+
+    /**
+     * @param emailTestAddress the emailTestAddress to set
+     */
+    public void setEmailTestAddress(String emailTestAddress) {
+        this.emailTestAddress = emailTestAddress;
+    }
+
+    /**
+     * @return the emailTestSubject
+     */
+    public String getEmailTestSubject() {
+        return emailTestSubject;
+    }
+
+    /**
+     * @param emailTestSubject the emailTestSubject to set
+     */
+    public void setEmailTestSubject(String emailTestSubject) {
+        this.emailTestSubject = emailTestSubject;
+    }
+
+    /**
+     * @return the emailTestMessage
+     */
+    public String getEmailTestMessage() {
+        return emailTestMessage;
+    }
+
+    /**
+     * @param emailTestMessage the emailTestMessage to set
+     */
+    public void setEmailTestMessage(String emailTestMessage) {
+        this.emailTestMessage = emailTestMessage;
+    }
+    
+    /**
+     * @return the testEmailResult
+     */
+    public String getTestEmailResult() {
+        return testEmailResult;
+    }
+
+    /**
+     * @param testEmailResult the testEmailResult to set
+     */
+    public void setTestEmailResult(String testEmailResult) {
+        this.testEmailResult = testEmailResult;
+    }
+
+    /**
+     * 
+     */
+    public String sendTestEmail() {
+        log.info("Testing email configuration, with an email to: "+this.emailTestAddress);
+        /*
+        UserManager um = UserBean.getUserManager();
+        um.sendUserMessage(username, this.emailTestSubject, this.emailTestMessage)
+        */
+        try {
+            // Send a message.
+            PlanetsMailMessage mailer = new PlanetsMailMessage();
+            mailer.setSubject(this.emailTestSubject);
+            mailer.setBody(this.emailTestMessage);
+            mailer.addRecipient(this.emailTestAddress);
+            mailer.send();
+            this.setTestEmailResult("You email appears to have been successfully sent. Please check the logs.");
+        } catch( Exception e ) {
+            this.setTestEmailResult("Sending email failed, with exception: "+e);
+        }
+        return "success";
+    }
 
 }
