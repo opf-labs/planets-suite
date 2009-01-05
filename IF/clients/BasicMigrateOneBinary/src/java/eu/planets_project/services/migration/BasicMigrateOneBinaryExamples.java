7b8df5297f19adbb2299db326cb8a2afdee7815a
carlwilson-bl <carl.wilson@bl.uk>
2009-01-05 10:40:44 +0000

    Added javadoc comments to reduce warning count. Deliberately left deprecated warnings as we'll need to catch these.

--- IF/clients/BasicMigrateOneBinary/src/java/eu/planets_project/services/migration/BasicMigrateOneBinaryExamples.java
+++ IF/clients/BasicMigrateOneBinary/src/java/eu/planets_project/services/migration/BasicMigrateOneBinaryExamples.java
@@ -1,19 +1,15 @@
 package eu.planets_project.services.migration;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
 import javax.xml.ws.BindingProvider;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Response;
+import javax.xml.ws.Service;
 
 import eu.planets_project.services.PlanetsException;
 import eu.planets_project.services.clients.BasicMigrateOneBinaryClient;
@@ -30,6 +26,11 @@ import eu.planets_project.services.migrate.BasicMigrateOneBinary;
  */
 public class BasicMigrateOneBinaryExamples {
 
+    /**
+     * @param args
+     * @throws IOException
+     * @throws PlanetsException
+     */
     public static void main(String[] args) throws IOException, PlanetsException {
     	
     	/* Please comment out/in the wsdl-location of the service you wish to test: */
@@ -131,6 +132,11 @@ public class BasicMigrateOneBinaryExamples {
     }
 
 
+    /**
+     * @param args
+     * @throws IOException
+     * @throws PlanetsException
+     */
     public static void mainXena(String[] args) throws IOException, PlanetsException {
 
         File docFile = new File("C:/test.doc");
