4b3c7cfdd4932ae1affd915ae7a7ae95b6787590
carlwilson-bl <carl.wilson@bl.uk>
2009-01-05 15:29:11 +0000

    A little refactoring to remove some unnecessary code.

--- IF/registry/src/main/java/eu/planets_project/ifr/core/registry/impl/Endpoint.java
+++ IF/registry/src/main/java/eu/planets_project/ifr/core/registry/impl/Endpoint.java
@@ -1,14 +1,10 @@
 package eu.planets_project.ifr.core.registry.impl;
 
-import java.net.URI;
 import java.net.URL;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import eu.planets_project.ifr.core.registry.api.Registry;
-import eu.planets_project.ifr.core.registry.utils.DiscoveryUtils;
 import eu.planets_project.ifr.core.registry.utils.PlanetsServiceExplorer;
 import eu.planets_project.services.datatypes.ServiceDescription;
 
@@ -19,18 +15,19 @@ import eu.planets_project.services.datatypes.ServiceDescription;
 public class Endpoint {
 	private static Log log = LogFactory.getLog(Endpoint.class);
 	
-	// This is a bodge to work around the
+	// This is a bodge to work around a bool read problem in JSF
 	private static final String notRegGraphic = "/images/notreg.gif"; 
 	private static final String isRegGraphic = "/images/reg.gif"; 
-	// private
 	// properties
 	private URL location = null;
 	private String name = "";
+	private String fullName = "";
 	private String category = "";
 	private String type = "";
 	private String status = "Not tested";
-	private boolean depracated = false;
+	private boolean deprecated = false;
 	private boolean registered = false;
+	// Part 2 of the great JSF bool bug bodge
 	private String regGraphic = notRegGraphic;
 	/**
 	 * We don't want Endpoints without URLs so disable no arg 
@@ -45,13 +42,16 @@ public class Endpoint {
 	 */
 	public Endpoint(PlanetsServiceExplorer pse) {
 		this.location = pse.getWsdlLocation();
-		// TODO name is just set from path, some extra work needed
+		// FIXME: name is just set from path, some extra work needed
 		this.name = this.location.getPath();
 		if (this.name.lastIndexOf('/') >= 0) {
 			this.name = name.substring(this.name.lastIndexOf('/') + 1);
 		}
 		if (null != pse.getServiceClass()) {
+			log.info("pse.getServiceClass()->" + pse.getServiceClass().getCanonicalName());
+			log.info("pse.getQName()->" + pse.getQName().toString());
 			this.type = pse.getServiceClass().getCanonicalName();
+			this.fullName = this.type;
 			if (pse.getServiceClass().getCanonicalName().indexOf('.') >= 0) {
 				this.setCategory(pse.getServiceClass().getCanonicalName().substring(pse.getServiceClass().getCanonicalName().lastIndexOf('.') + 1));
 			}
@@ -63,14 +63,14 @@ public class Endpoint {
 				else this.status = "Failed";
 			} catch (RuntimeException e) {
 				this.status = "Unknown";
-				this.depracated = true;
+				this.deprecated = true;
 			}
 		}
 	}
 	
 	/**
+	 * Construct an Endpoint from a ServiceDescription
 	 * @param desc 
-	 * 
 	 */
 	public Endpoint(ServiceDescription desc) {
 		this.location = desc.getEndpoint();
@@ -78,7 +78,7 @@ public class Endpoint {
 		this.setCategory(desc.getType().substring(desc.getType().lastIndexOf('.') + 1));
 		this.type = desc.getType();
 		this.status = "OK";
-		this.depracated = false;
+		this.deprecated = false;
 		this.registered = true;
 		this.regGraphic = isRegGraphic;
 	}
@@ -108,6 +108,20 @@ public class Endpoint {
 	 */
 	public String getName() {
 		return name;
+	}
+
+	/**
+	 * @param fullName the fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
 	}
 
 	/**
@@ -142,14 +156,14 @@ public class Endpoint {
 	 * @param depracated the described to set
 	 */
 	public void setDepracated(boolean depracated) {
-		this.depracated = depracated;
+		this.deprecated = depracated;
 	}
 
 	/**
 	 * @return the described
 	 */
 	public boolean isDepracated() {
-		return depracated;
+		return deprecated;
 	}
 
 	/**
