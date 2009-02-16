705f9bf677efc4216a6334511795569e911ccce6
Claus Jensen <cjen@kb.dk>
2009-02-16 10:01:59 +0000

    Made some minor updates to the pdf2html test component.

--- PA/pdf2html/test/java/eu/planets_project/services/migration/pdf2html/Pdf2HtmlMigrationTest.java
+++ PA/pdf2html/test/java/eu/planets_project/services/migration/pdf2html/Pdf2HtmlMigrationTest.java
@@ -1,6 +1,5 @@
 package eu.planets_project.services.migration.pdf2html;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URI;
 import java.io.File;
@@ -13,28 +12,32 @@ import org.junit.Test;
 import eu.planets_project.services.datatypes.Content;
 import eu.planets_project.services.datatypes.DigitalObject;
 import eu.planets_project.services.datatypes.ServiceDescription;
-import eu.planets_project.services.datatypes.MigrationPath;
 import eu.planets_project.services.migrate.Migrate;
 import eu.planets_project.services.migrate.MigrateResult;
 import eu.planets_project.services.utils.FileUtils;
 import eu.planets_project.services.utils.test.ServiceCreator;
-import eu.planets_project.ifr.core.techreg.api.formats.Format;
-import eu.planets_project.ifr.core.techreg.api.formats.FormatRegistry;
-import eu.planets_project.ifr.core.techreg.api.formats.FormatRegistryFactory;
 
 /**
  * Local and client tests of the digital object migration functionality.
- * @author Fabian Steeg
+ * @author Asger Blekinge-Rasmussen <abr@statsbiblioteket.dk>, Claus Jensen <cjen@kb.dk>
  */
 public final class Pdf2HtmlMigrationTest extends TestCase {
 
-    /* The location of this service when deployed. */
-    String wsdlLoc = "/pserv-pa-pdf2html/Pdf2HtmlMigration?wsdl";
+    /**
+     * The location of this service when deployed.
+     */
+    private String wsdlLoc = "/pserv-pa-pdf2html/Pdf2HtmlMigration?wsdl";
 
-    /* A holder for the object to be tested */
-    Migrate dom = null;
+    /**
+     * A holder for the object to be tested.
+     */
+    private Migrate dom = null;
 
-    File testpdf = new File("PA/pdf2html/test/resources/test.pdf");
+    /**
+     * A test file object.
+     */
+    private File testpdf = new File("PA/pdf2html/test/resources/test.pdf");
+
     /*
      * (non-Javadoc)
      * @see junit.framework.TestCase#setUp()
@@ -77,8 +80,6 @@ public final class Pdf2HtmlMigrationTest extends TestCase {
         * To test usability of the digital object instance in web services,
 * we simply pass one into the service and expect one back:
 */
-            FormatRegistry fm= FormatRegistryFactory.getFormatRegistry();
-            MigrationPath[] paths = MigrationPath.constructPaths(fm.getURIsForExtension("ps"),fm.getURIsForExtension("pdf"));
 
             DigitalObject input =
                     new DigitalObject.Builder(Content.byValue(testpdf))
@@ -88,17 +89,17 @@ public final class Pdf2HtmlMigrationTest extends TestCase {
                             build();
             System.out.println("Input: " + input);
 
-            MigrateResult mr = dom.migrate(input, null,null, null);
+            MigrateResult mr = dom.migrate(input, null, null, null);
             DigitalObject doOut = mr.getDigitalObject();
 
             assertTrue("Resulting digital object is null.", doOut != null);
 
             System.out.println("Output: " + doOut);
-            
+
             Content content = doOut.getContent();
-            
+
             File workfolder = FileUtils.createWorkFolderInSysTemp("pdf2html_test");
-            
+
             File resultHtml = FileUtils.writeInputStreamToFile(content.read(), workfolder, "pdf2html_result.html");
             
             System.out.println("Please find the result HTML file here: \n" + resultHtml.getAbsolutePath());
--- PA/pdf2html/test/java/eu/planets_project/services/migration/pdf2html/Pdf2HtmlMigrationTestSuite.java
+++ PA/pdf2html/test/java/eu/planets_project/services/migration/pdf2html/Pdf2HtmlMigrationTestSuite.java
@@ -1,12 +1,11 @@
-/**
- *
- */
 package eu.planets_project.services.migration.pdf2html;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
+/**
+ * Pdf2Html service test suite.
+ */
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { Pdf2HtmlMigrationTest.class })
-public class Pdf2HtmlMigrationTestSuite {}
+@Suite.SuiteClasses({ Pdf2HtmlMigrationTest.class })
+public class Pdf2HtmlMigrationTestSuite { }
