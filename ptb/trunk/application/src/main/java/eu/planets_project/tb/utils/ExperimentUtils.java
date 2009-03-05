a975cd3278c65f2fb07327fb5e5d0e626b880a9f
Andrew Jackson <Andrew.Jackson@bl.uk>
2009-03-05 15:35:56 +0000

    Added DO# to the CSV.

--- ptb/trunk/application/src/main/java/eu/planets_project/tb/utils/ExperimentUtils.java
+++ ptb/trunk/application/src/main/java/eu/planets_project/tb/utils/ExperimentUtils.java
@@ -40,7 +40,7 @@ public class ExperimentUtils {
     public static void outputResults( OutputStream os, String expId, DATA_FORMAT format ) throws IOException {
         log.info("Writing out experiment "+expId+" as "+format);
 
-        Writer out = new BufferedWriter( new OutputStreamWriter( os ) );
+        Writer out = new BufferedWriter( new OutputStreamWriter( os, "UTF-8" ) );
         CSVWriter writer = new CSVWriter(out);
 
         long id = Long.parseLong(expId);
@@ -49,21 +49,23 @@ public class ExperimentUtils {
         Experiment exp = edao.findExperiment(id);
 
         // The string array 
-        String sa[] = new String[7];
+        String sa[] = new String[8];
         sa[0] = "Name";
         sa[1] = "Run #";
         sa[2] = "Date";
-        sa[3] = "Digital Object";
-        sa[4] = "Stage";
-        sa[5] = "Property Identifier";
-        sa[6] = "Property Value";
+        sa[3] = "Digital Object #";
+        sa[4] = "Digital Object Source";
+        sa[5] = "Stage";
+        sa[6] = "Property Identifier";
+        sa[7] = "Property Value";
         // write the headers out:
         writer.writeNext(sa);
         
         // Loop through:
+        int bi = 1;
         for( BatchExecutionRecordImpl batch : exp.getExperimentExecutable().getBatchExecutionRecords() ) {
             // log.info("Found batch... "+batch);
-            int i = 1;
+            int doi = 1;
             for( ExecutionRecordImpl exr : batch.getRuns() ) {
                 // log.info("Found Record... "+exr+" stages: "+exr.getStages());
                 if( exr != null && exr.getStages() != null ) {
@@ -72,21 +74,24 @@ public class ExperimentUtils {
                         for( MeasurementRecordImpl m : exsr.getMeasurements() ) {
                             // log.info("Looking at result for property "+m.getIdentifier());
                             sa[0] = exp.getExperimentSetup().getBasicProperties().getExperimentName();
-                            sa[1] = "Run " + i;
+                            sa[1] = "" + bi;
                             sa[2] = batch.getStartDate().getTime().toString();
-                            sa[3] = exr.getDigitalObjectSource();
-                            sa[4] = exsr.getStage();
-                            sa[5] = m.getIdentifier();
-                            sa[6] = m.getValue();
+                            sa[3] = "" + doi;
+                            sa[4] = exr.getDigitalObjectSource();
+                            sa[5] = exsr.getStage();
+                            sa[6] = m.getIdentifier();
+                            sa[7] = m.getValue();
                             // Write out CSV:
                             writer.writeNext( sa );
                         }
                     }
                 }
-                // Increment, for the next run.
-                i++;
+                // Increment, for the next DO.
+                doi++;
                 out.flush();
             }
+            // Increment to the next batch:
+            bi++;
         }
 
     }
