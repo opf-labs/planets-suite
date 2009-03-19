4035bbb2fbc940b4311648d3fcd233c5e49ebe36
Thomas Skou Hansen <tsh@statsbiblioteket.dk>
2009-03-19 14:15:53 +0000

    Added the Dia migration service.

---
+++ PA/dia/src/java/eu/planets_project/services/migration/dia/impl/DiaMigrationService.java
@@ -0,0 +1,405 @@
+package eu.planets_project.services.migration.dia.impl;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import eu.planets_project.services.PlanetsServices;
+import eu.planets_project.services.datatypes.DigitalObject;
+import eu.planets_project.services.datatypes.MigrationPath;
+import eu.planets_project.services.datatypes.Parameters;
+import eu.planets_project.services.datatypes.ServiceDescription;
+import eu.planets_project.services.datatypes.ServiceReport;
+import eu.planets_project.services.migrate.Migrate;
+import eu.planets_project.services.migrate.MigrateResult;
+import eu.planets_project.services.utils.PlanetsLogger;
+import eu.planets_project.services.utils.cli.CliMigrationPaths;
+
+/**
+ * DiaMigrationService testing service.
+ * 
+ * @author Bolette Ammitzbøll Jurik (bam@statsbiblioteket.dk)
+ * @author Thomas Skou Hansen (tsh@statsbiblioteket.dk)
+ */
+@Local(Migrate.class)
+@Remote(Migrate.class)
+@Stateless
+
+@WebService(name = DiaMigrationService.NAME, 
+		serviceName = Migrate.NAME, 
+		targetNamespace = PlanetsServices.NS,
+		endpointInterface = "eu.planets_project.services.migrate.Migrate" )
+
+public final class DiaMigrationService implements Migrate, Serializable {
+
+	/** The service name */
+	static final String NAME = "DiaMigrationService";
+
+    static final String configfile = "dia.paths.xml";
+
+	/** The unique class id */
+	private static final long serialVersionUID = 4596228292063217306L;
+
+	private PlanetsLogger log = PlanetsLogger.getLogger(DiaMigrationService.class);
+
+	private CliMigrationPaths migrationPaths = null;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see eu.planets_project.services.migrate.Migrate#migrate(eu.planets_project.services.datatypes.DigitalObject, java.net.URI, java.net.URI, eu.planets_project.services.datatypes.Parameters)
+	 */
+	public MigrateResult migrate( final DigitalObject digitalObject, URI inputFormat,
+			URI outputFormat, Parameters parameters) {
+		/*
+		 * We just return a new digital object with the same required arguments
+		 * as the given:
+		 */
+		DigitalObject newDO = DigitalObject.of(digitalObject.toXml());
+		ServiceReport report = new ServiceReport();
+		return new MigrateResult(newDO, report);
+	}
+
+
+	/**
+	 * @see eu.planets_project.services.migrate.Migrate#describe()
+	 */
+	public ServiceDescription describe() {
+
+		try {
+			final Set<URI> inputFormatURIs = getAllowedInputFormatURIs();
+			final Set<URI> outputFormatURIs = getAllowedOutputFormatURIs();
+
+			ServiceDescription.Builder serviceDescriptionBuilder = new ServiceDescription.Builder(NAME, Migrate.class.getCanonicalName());
+			serviceDescriptionBuilder.classname(this.getClass().getCanonicalName());
+			serviceDescriptionBuilder.description("File migration service using Dia.");
+			serviceDescriptionBuilder.author("Bolette Ammitzbøll Jurik <bam@statsbiblioteket.dk>, Thomas Skou Hansen <tsh@statsbiblioteket.dk>");
+			//		serviceDescriptionBuilder.furtherInfo(null);
+			serviceDescriptionBuilder.identifier(null);
+
+
+//			serviceDescriptionBuilder.inputFormats(null);
+			//		serviceDescriptionBuilder.instructions(null);
+			//		serviceDescriptionBuilder.name(null);
+			//		serviceDescriptionBuilder.parameters(null);
+
+
+			outputFormatURIs.add(new URI("info:pronom/fmt/91"));
+			outputFormatURIs.add(new URI("info:pronom/fmt/92"));
+
+
+
+
+
+
+
+			final MigrationPath[] migrationPaths = MigrationPath.constructPaths(inputFormatURIs, outputFormatURIs);
+			serviceDescriptionBuilder.paths(migrationPaths);
+			//		serviceDescriptionBuilder.properties(null);
+			//		serviceDescriptionBuilder.serviceProvider(null);
+			//		serviceDescriptionBuilder.tool(null);
+			//		serviceDescriptionBuilder.type(null);
+			//		serviceDescriptionBuilder.version(null);
+
+			return serviceDescriptionBuilder.build();
+		} catch (URISyntaxException urise) {
+			throw new Error("Failed building migration path information.", urise);
+		}
+	}
+
+	private Set<URI> getAllowedInputFormatURIs() throws URISyntaxException {
+
+		final HashSet<URI> inputFormatURIs = new HashSet<URI>();
+
+		// DIA URI
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/381"));
+
+		// SVG URIs
+		inputFormatURIs.add(new URI("info:pronom/fmt/91"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/92"));
+
+		// PNG URIs
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/11"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/12"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/13"));
+
+		// DXF URIs
+		inputFormatURIs.add(new URI("info:pronom/fmt/63"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/64"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/65"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/66"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/67"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/68"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/69"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/70"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/71"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/72"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/73"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/74"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/75"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/76"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/77"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/78"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/79"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/80"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/81"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/82"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/83"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/84"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/85"));
+
+		// TODO: FIG URIs are not provided by PRONOM. Add these when possible.
+
+		// BMP URIs
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/25"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/114"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/115"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/116"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/117"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/118"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/119"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/270"));
+
+
+		// GIF URIs
+		inputFormatURIs.add(new URI("info:pronom/fmt/3"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/4"));
+
+		// TODO: PNM URIs are not provided by PRONOM. Add these when possible.
+
+		// RAS URI
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/184"));
+
+		// TIF URIs
+		inputFormatURIs.add(new URI("info:pronom/fmt/7"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/8"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/9"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/10"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/152"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/153"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/154"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/155"));
+		inputFormatURIs.add(new URI("info:pronom/fmt/156"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/399"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/387"));
+		inputFormatURIs.add(new URI("info:pronom/x-fmt/388"));
+
+		return inputFormatURIs;
+	}
+
+	private Set<URI> getAllowedOutputFormatURIs() throws URISyntaxException {
+
+		final HashSet<URI> outputFormatURIs = new HashSet<URI>();
+
+		// CGM URI
+		outputFormatURIs.add(new URI("info:pronom/x-fmt/142"));
+
+		// DIA URI
+		outputFormatURIs.add(new URI("info:pronom/x-fmt/381"));
+
+		// TODO: SHAPE URI is not provided by PRONOM. Add when possible.
+
+		// PNG URIs
+		outputFormatURIs.add(new URI("info:pronom/fmt/11"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/12"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/13"));
+
+		// DXF URIs
+		outputFormatURIs.add(new URI("info:pronom/fmt/63"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/64"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/65"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/66"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/67"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/68"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/69"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/70"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/71"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/72"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/73"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/74"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/75"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/76"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/77"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/78"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/79"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/80"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/81"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/82"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/83"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/84"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/85"));
+
+		// PLT URI
+		outputFormatURIs.add(new URI("info:pronom/x-fmt/83"));
+
+		// HPGL URI
+		outputFormatURIs.add(new URI("info:pronom/fmt/293"));
+
+		// EPS URIs
+		outputFormatURIs.add(new URI("info:pronom/fmt/122"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/123"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/124"));
+
+		// TODO: EPSI URI is not provided by PRONOM. Add when possible.
+
+		// SVG URIs
+		outputFormatURIs.add(new URI("info:pronom/fmt/91"));
+		outputFormatURIs.add(new URI("info:pronom/fmt/92"));
+
+		// SVGZ URI
+		outputFormatURIs.add(new URI("info:pronom/x-fmt/109"));
+
+		// TODO: MP URI is not provided by PRONOM. Add when possible.
+
+		// TODO: TEX URI is not provided by PRONOM. Add when possible.
+
+		// WPG URI
+		outputFormatURIs.add(new URI("info:pronom/x-fmt/395"));
+
+		// TODO: FIG URI is not provided by PRONOM. Add when possible.
+
+		// TODO: CODE URI is not provided by PRONOM. Add when possible.
+
+		return outputFormatURIs;
+	}
+
+
+	private void initialiseMigrationPaths() throws URISyntaxException {
+		try {
+			if (migrationPaths == null){
+				migrationPaths = CliMigrationPaths.initialiseFromFile(configfile);
+			}
+		} catch (ParserConfigurationException e) {
+			throw new Error("Not supposed to happen",e);
+		} catch (IOException e) {
+			throw new Error("Not supposed to happen",e);
+		} catch (SAXException e) {
+			throw new Error("Not supposed to happen",e);
+		}
+	}
+
+	// 
+	//    private CliMigrationPaths migrationPaths = null;
+	//
+	//    /**
+	//     * {@inheritDoc}
+	//     *
+	//     * @see eu.planets_project.services.migrate.Migrate#migrate(eu.planets_project.services.datatypes.DigitalObject, java.net.URI, java.net.URI, eu.planets_project.services.datatypes.Parameters)
+	//     */
+	//    public MigrateResult migrate(final DigitalObject digitalObject,
+	//                                 URI inputFormat, URI outputFormat, Parameters parameters) {
+	//
+	//
+	//        ServiceReport report = new ServiceReport();
+	//        try {
+	//            init();
+	//        } catch (URISyntaxException e) {
+	//            log.error("Invalid URI in the paths file",e);
+	//            return fail(null);
+	//        }
+	//
+	//
+	//        String command = migrationPaths.findMigrationCommand(inputFormat,outputFormat);
+	//
+	//        if (command == null){
+	//            report.setError("Could not find a migrationPath for the input and output formats");
+	//            return fail(report);
+	//        }
+	//
+	//
+	//        //log.info("Using ps2pdf application name: "+this.ps2pdf_app_name);
+	//
+	//
+	//        /*
+	//         * We just return a new digital object with the same required arguments
+	//         * as the given:
+	//         */
+	//
+	//
+	//        InputStream psfile = digitalObject.getContent().read();
+	//
+	//
+	//
+	//        ProcessRunner runner = new ProcessRunner();
+	//
+	//        runner.setCommand(Arrays.asList("/bin/sh","-c",command));
+	//
+	//        runner.setInputStream(psfile);
+	//        runner.setCollection(true);
+	//        runner.setOutputCollectionByteSize(-1);
+	//
+	//
+	//
+	//        runner.run();
+	//        int return_code = runner.getReturnCode();
+	//
+	//
+	//
+	//        if (return_code != 0){
+	//            report.setErrorState(return_code);
+	//            report.setError(runner.getProcessOutputAsString()+"\n"+runner.getProcessErrorAsString());
+	//            return fail(report);
+	//        }
+	//        InputStream newFileStream = runner.getProcessOutput();
+	//        byte[] outbytes = FileUtils.writeInputStreamToBinary(newFileStream);
+	//
+	//        DigitalObject pdfFile = new DigitalObject.Builder(Content.byValue(outbytes)).build();
+	//        return new MigrateResult(pdfFile,report);
+	//
+	//    }
+	//
+	//    /**
+	//     * @see eu.planets_project.services.migrate.Migrate#describe()
+	//     * @return ServiceDescription
+	//     */
+	//    public ServiceDescription describe() {
+	//
+	//        ServiceDescription.Builder builder = new ServiceDescription.Builder(NAME, Migrate.class.getName());
+	//        try {
+	//            init();
+	//            builder.paths(migrationPaths.getAsPlanetsPaths());
+	//        } catch (URISyntaxException e) {
+	//            log.warn("Invalid URI in the paths file",e);
+	//        }
+	//
+	//        builder.author("Asger Blekinge-Rasmussen <abr@statsbiblioteket.dk>");
+	//        builder.classname(this.getClass().getCanonicalName());
+	//        builder.description("Converts between a number of image formats");
+	//
+	//        builder.version("0.1");
+	//
+	//        return builder.build();
+	//
+	//    }
+	//
+	//    private void init() throws URISyntaxException {
+	//        try {
+	//            if (migrationPaths == null){
+	//                migrationPaths = CliMigrationPaths.initialiseFromFile(configfile);
+	//            }
+	//        } catch (ParserConfigurationException e) {
+	//            throw new Error("Not supposed to happen",e);
+	//        } catch (IOException e) {
+	//            throw new Error("Not supposed to happen",e);
+	//        } catch (SAXException e) {
+	//            throw new Error("Not supposed to happen",e);
+	//        }
+	//    }
+	//
+	//
+	//    private MigrateResult fail(ServiceReport report){
+	//        return new MigrateResult(null,report);
+	//    }
+
+}
