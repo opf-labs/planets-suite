34ecb26af4483dc6002eab1fd2b75af09b6cc78f
Fabian Steeg <fsteeg@gmail.com>
2009-04-21 13:28:05 +0000

    Set up basic format mapping tests in the techreg component.

---
+++ IF/techreg/src/test/java/eu/planets_project/ifr/core/techreg/AllTechregSuite.java
@@ -0,0 +1,16 @@
+package eu.planets_project.ifr.core.techreg;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import eu.planets_project.ifr.core.techreg.api.formats.FormatMappingTests;
+
+/**
+ * All tests for the techreg component.
+ * @author Fabian Steeg (fabian.steeg@uni-koeln.de)
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(FormatMappingTests.class)
+public class AllTechregSuite {
+}
