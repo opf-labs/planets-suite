c52b2a6df550a2affde5c7581e70bb83d2570ded
Ross King <ross.king@ait.ac.at>
2009-04-28 07:09:44 +0000

    updated for new ImmutableContent

--- components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/api/data/DigitalObjectMultiManager.java
+++ components/wdt/src/main/java/eu/planets_project/ifr/core/wdt/api/data/DigitalObjectMultiManager.java
@@ -12,6 +12,7 @@ import eu.planets_project.ifr.core.storage.api.DataManagerLocal;
 import eu.planets_project.ifr.core.storage.api.DigitalObjectManager;
 import eu.planets_project.services.datatypes.Content;
 import eu.planets_project.services.datatypes.DigitalObject;
+import eu.planets_project.services.datatypes.ImmutableContent;
 
 /**
  * This class managers all of the Data Registries known to the Testbed.
@@ -141,7 +142,7 @@ public class DigitalObjectMultiManager implements DigitalObjectManager {
         DigitalObject.Builder dob;
         
         try {
-            dob = new DigitalObject.Builder( Content.byValue( dm.retrieveBinary(pdURI)) );
+            dob = new DigitalObject.Builder(ImmutableContent.byValue( dm.retrieveBinary(pdURI)) );
         } catch (SOAPException e1) {
             e1.printStackTrace();
             log.error("Could not retrieve the binary for " + pdURI);
