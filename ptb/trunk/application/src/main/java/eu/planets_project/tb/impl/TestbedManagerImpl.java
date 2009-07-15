a4738be11f80d0efaceebdfa5308d2092cd56d5a
Andrew Jackson <Andrew.Jackson@bl.uk>
2009-07-15 10:32:29 +0000

    Updated to say v0.9.5.

--- ptb/trunk/application/src/main/java/eu/planets_project/tb/impl/TestbedManagerImpl.java
+++ ptb/trunk/application/src/main/java/eu/planets_project/tb/impl/TestbedManagerImpl.java
@@ -55,7 +55,7 @@ public class TestbedManagerImpl
 	ExperimentPersistencyRemote edao;
 	
 	// The version number of the Testbed.  Can be overridden in BackendResources.properties.
-	private String tbVersion = "0.9";
+	private String tbVersion = "0.9.5";
 	
     // Look for the batch system... 
     TestbedBatchProcessor tbp = (TestbedBatchProcessor)JSFUtil.getManagedObject("TestbedBatchProcessor");
