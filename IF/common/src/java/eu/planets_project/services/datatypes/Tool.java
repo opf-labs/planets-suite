c6812e0704e8a888fce4675f33529be903580b42
Andrew Jackson <Andrew.Jackson@bl.uk>
2009-10-28 15:38:44 +0000

    Removed erroneous copyright header.

--- IF/common/src/java/eu/planets_project/services/datatypes/Tool.java
+++ IF/common/src/java/eu/planets_project/services/datatypes/Tool.java
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2007, 2008 The Planets Project Partners. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the GNU Lesser General Public License v3 which accompanies this
- * distribution, and is available at http://www.gnu.org/licenses/lgpl.html
- */
 package eu.planets_project.services.datatypes;
 
 import eu.planets_project.services.PlanetsServices;
