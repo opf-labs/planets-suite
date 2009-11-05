d5c1eed02ea827d769b6de6578054e78edce3fe0
Roman Graf <roman.graf@ait.ac.at>
2009-11-05 16:16:44 +0000

    WEE_DATA path changed

--- ifsp/trunk/components/wee/src/main/java/eu/planets_project/ifr/core/wee/api/WorkflowReporter.java
+++ ifsp/trunk/components/wee/src/main/java/eu/planets_project/ifr/core/wee/api/WorkflowReporter.java
@@ -19,7 +19,7 @@ final class WorkflowReporter {
     private static final String TEMPLATE = "ReportTemplate.html";
     private static final String CONTENT_MARKER = "###CONTENT###";
     private static final String LOCAL = "components/wee/src/main/resources/";
-    private static final String WEE_DATA = "/server/default/deploy/jboss-web.deployer/ROOT.war/data/wee";
+    private static final String WEE_DATA = "/planets-ftp/gen";
     private static final String JBOSS_HOME_DIR_KEY = "jboss.home.dir";
     private static final String JBOSS_HOME = System.getProperty(JBOSS_HOME_DIR_KEY);
     private static final String ENTRY =
