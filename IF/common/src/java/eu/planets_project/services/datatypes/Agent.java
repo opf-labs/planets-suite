c7a44004db63aa650916969d1ceedeb128e74520
carlwilson-bl <carl.wilson@bl.uk>
2009-11-12 11:58:12 +0000

    Just some code tidying.

--- IF/common/src/java/eu/planets_project/services/datatypes/Agent.java
+++ IF/common/src/java/eu/planets_project/services/datatypes/Agent.java
@@ -10,15 +10,20 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * Planets agent representation.
- * <p>Instances of this class are immutable and so can be shared freely.</p>
+ * <p>
+ * Instances of this class are immutable and so can be shared freely.
+ * </p>
  * @author <a href="mailto:Andrew.Jackson@bl.uk">Andy Jackson</a>
  */
 @XmlType(namespace = PlanetsServices.OBJECTS_NS)
 public final class Agent {
 
-	@XmlAttribute private String id;
-	@XmlAttribute private String name;
-	@XmlAttribute private String type;
+    @XmlAttribute
+    private String id;
+    @XmlAttribute
+    private String name;
+    @XmlAttribute
+    private String type;
 
     /**
      * For JAXB.
@@ -56,5 +61,59 @@ public final class Agent {
         this.id = id;
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Agent other = (Agent) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
 }
