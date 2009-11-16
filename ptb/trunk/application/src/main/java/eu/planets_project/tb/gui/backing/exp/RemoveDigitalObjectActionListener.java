fe9de9cde9d94f72b565c9026690131cc64310cc
carlwilson-bl <carl.wilson@bl.uk>
2009-11-16 15:39:57 +0000

    Reorganized import statements.

--- ptb/trunk/application/src/main/java/eu/planets_project/tb/gui/backing/exp/RemoveDigitalObjectActionListener.java
+++ ptb/trunk/application/src/main/java/eu/planets_project/tb/gui/backing/exp/RemoveDigitalObjectActionListener.java
@@ -11,7 +11,7 @@ import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 
-import eu.planets_project.ifr.core.common.logging.PlanetsLogger;
+import eu.planets_project.services.utils.PlanetsLogger;
 import eu.planets_project.tb.gui.backing.ExperimentBean;
 import eu.planets_project.tb.gui.util.JSFUtil;
 
