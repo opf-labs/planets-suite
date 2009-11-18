51e8d7e7e8000659bd732e26a374743cbb2c26c0
carlwilson-bl <carl.wilson@bl.uk>
2009-11-18 16:59:48 +0000

    Refactored techreg to overcome circular build dependencies.  Moved some elements to common and added an api package for FormatRegistry class.

--- PA/floppyImageHelper/src/java/eu/planets_project/services/modification/floppyImageModify/impl/FloppyImageModifyWin.java
+++ PA/floppyImageHelper/src/java/eu/planets_project/services/modification/floppyImageModify/impl/FloppyImageModifyWin.java
@@ -14,7 +14,7 @@ import javax.xml.ws.BindingType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import eu.planets_project.ifr.core.techreg.formats.FormatRegistry;
+import eu.planets_project.ifr.core.techreg.formats.api.FormatRegistry;
 import eu.planets_project.ifr.core.techreg.formats.FormatRegistryFactory;
 import eu.planets_project.services.PlanetsServices;
 import eu.planets_project.services.datatypes.Content;
