d913c83b9b24e1c6185c8cfc6c060e4c1a74c249
carlwilson-bl <carl.wilson@bl.uk>
2009-11-18 16:57:55 +0000

    Reflecting package namespace change in techreg, introduced an api package.

--- IF/registry/src/test/java/eu/planets_project/ifr/core/registry/api/CoreRegistryTests.java
+++ IF/registry/src/test/java/eu/planets_project/ifr/core/registry/api/CoreRegistryTests.java
@@ -12,8 +12,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import eu.planets_project.ifr.core.techreg.formats.FormatRegistry;
 import eu.planets_project.ifr.core.techreg.formats.FormatRegistryFactory;
+import eu.planets_project.ifr.core.techreg.formats.api.FormatRegistry;
 import eu.planets_project.services.datatypes.Property;
 import eu.planets_project.services.datatypes.ServiceDescription;
 
