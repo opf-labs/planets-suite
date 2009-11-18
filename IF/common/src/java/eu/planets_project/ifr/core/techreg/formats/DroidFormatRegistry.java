6f48dfc77381191f6087be2952f7a83d9852a8e1
carlwilson-bl <carl.wilson@bl.uk>
2009-11-18 16:56:34 +0000

    Added some techreg components to common to overcome a circular build dependency.

---
+++ IF/common/src/java/eu/planets_project/ifr/core/techreg/formats/DroidFormatRegistry.java
@@ -0,0 +1,139 @@
+/**
+ * 
+ */
+package eu.planets_project.ifr.core.techreg.formats;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.util.Set;
+import java.util.HashSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import uk.gov.nationalarchives.droid.AnalysisController;
+import uk.gov.nationalarchives.droid.signatureFile.FFSignatureFile;
+import uk.gov.nationalarchives.droid.signatureFile.FileFormat;
+
+/**
+ * @author <a href="mailto:Andrew.Jackson@bl.uk">Andy Jackson</a>
+ *
+ */
+class DroidFormatRegistry  {
+    
+    private static Log log = LogFactory.getLog(DroidFormatRegistry.class);
+
+    private static final String INFO_PRONOM = "pronom/";
+    
+    FFSignatureFile sigFile;
+    
+    int numFormats = 0;
+    
+    /**
+     * Constructor to set up the look-up tables.
+     */
+    public DroidFormatRegistry() {
+        // Grab a Droid analyser:
+        AnalysisController controller = getController();
+        
+        // Get the list of file formats
+        sigFile = controller.getSigFile();
+        numFormats = sigFile.getNumFileFormats();
+
+    }
+    
+    /**
+     * 
+     * @param PUID The PUID string
+     * @return A URI representation of the given PUID
+     */
+    static URI PUIDtoURI( String PUID ) {
+        URI puidURI = null;
+        try {
+            // Opaque URL constructed using a Scheme Specific Part:
+            puidURI = new URI("info", INFO_PRONOM + PUID, null );
+        } catch (URISyntaxException e) {
+            log.error("Exception while constructing type URI: "+e);
+            puidURI = null;
+        }
+        return puidURI;
+    }
+    
+    /**
+     * 
+     * @param puri A planets URI
+     * @return The raw PUID
+     */
+    static String URItoPUID( URI puri ) {
+        if( puri == null ) return null;
+        return puri.getSchemeSpecificPart().substring(INFO_PRONOM.length());
+    }
+    
+    /**
+     * 
+     * @param ff The droid file format
+     * @return A Planets format created from the given droid format
+     */
+    private Format fillFormatFromPRONOM( FileFormat ff ) {
+        if( ff == null ) return null;
+        
+        // Store the unique id and the description
+        Format fmt  = new Format( PUIDtoURI(ff.getPUID()) );
+        try {
+            fmt.setRegistryURL( new URL("http://www.nationalarchives.gov.uk/PRONOM/Format/proFormatSearch.aspx?status=detailReport&id=" + ff.getID()) );
+        } catch (MalformedURLException e) {
+            fmt.setRegistryURL(null);
+        }
+        fmt.setSummary( ff.getName() );
+        fmt.setVersion( ff.getVersion() );
+        
+        // Store mime-type:
+        if( ff.getMimeType() != null && ! "".equals(ff.getMimeType()) ) {
+            HashSet<String> mimes = new HashSet<String>();
+            mimes.add(ff.getMimeType());
+            fmt.setMimeTypes(mimes);
+        }
+        
+        // Store extensions:
+        HashSet<String> exts = new HashSet<String>();
+        for( int j = 0; j < ff.getNumExtensions(); j++ ) 
+            exts.add(ff.getExtension(j));
+        fmt.setExtensions(exts);
+        
+        // Return
+        return fmt;
+    }
+    
+    /**
+     * 
+     * @return the formats in a Set
+     */
+    public Set<Format> getFormats() {
+       HashSet<Format> fmts = new HashSet<Format>();
+       for( int i = 0; i < sigFile.getNumFileFormats(); i++ ) {
+           fmts.add( fillFormatFromPRONOM(sigFile.getFileFormat(i)) );
+       }
+       return fmts;
+    }
+    
+    /**
+     * 
+     * @return the DROID analysis controller
+     */
+    public static AnalysisController getController() {
+        // Determine the config directory:
+        String sigFileLocation = DroidConfig.configFolder();
+        // Here we start using the Droid API:
+        AnalysisController controller = new AnalysisController();
+        try {
+            controller.readSigFile(sigFileLocation);
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+            return null;
+        }
+        return controller;
+    }
+}
---
+++ IF/common/src/java/eu/planets_project/ifr/core/techreg/formats/Format.java
@@ -0,0 +1,229 @@
+/**
+ * 
+ */
+package eu.planets_project.ifr.core.techreg.formats;
+
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is the Planets 'Preservation Object' 'Format' description entity. It is
+ * based on the DROID FileFormat entity, but simplified. Other format registries
+ * could be supported too. Note that DROID also provides: - 'has priority over'
+ * (good for doing PP) - 'signatures' (good for doing PC) but as this is meant
+ * to be a simple, interoperable type identifier system, this extra information
+ * (which is more oriented to specific use contexts) is not (yet) included here.
+ * Types are identified by URIs. As we are currently based on PRONOM, these URIs
+ * will be PRONOM info:pronom/fmt/XX URIs. By using these URIs, we retain the
+ * ability to add further registries in the future without changing any of this
+ * code.
+ * @author <a href="mailto:Andrew.Jackson@bl.uk">Andy Jackson</a>
+ */
+public class Format implements Serializable {
+
+    /**
+     * Different types of format URIs supported by Planets services.
+     * @author Fabian Steeg (fabian.steeg@uni-koeln.de)
+     */
+    public static enum UriType {
+        MIME, PRONOM, EXTENSION, ANY, UNKNOWN
+    }
+
+    /**
+     * A format URI representing any format, e.g. to define that a service can
+     * process any format.
+     */
+    public static final URI ANY = URI.create(FormatUtils.ANY_FORMAT);
+    
+
+    /**
+     * A format URI representing a "folder"
+     */
+    public static final URI FOLDER = URI.create(FormatUtils.FOLDER_TYPE);
+
+    /**
+     * A format URI representing an unknown format, e.g. to set the format of a
+     * digital object to be identified.
+     */
+    public static final URI UNKNOWN = URI.create(FormatUtils.UNKNOWN_FORMAT);
+
+    private static final long serialVersionUID = -4713590391811379383L;
+
+    private URI typeURI;
+
+    private Set<URI> aliases = new HashSet<URI>();
+
+    private URL registryURL;
+
+    private String summary;
+
+    private String version;
+
+    private Set<String> mimeTypes = new HashSet<String>();
+
+    private Set<String> extensions = new HashSet<String>();
+
+    /**
+     * @param typeURI The type URI (see FormatRegistry)
+     */
+    protected Format(URI typeURI) {
+        this.typeURI = typeURI;
+        if (FormatUtils.isMimeUri(typeURI)) {
+            String mime = typeURI.toString().replace(
+                    FormatUtils.MIME_URI_PREFIX, "");
+            this.mimeTypes = new HashSet<String>();
+            this.mimeTypes.add(mime);
+            this.summary = mime.toLowerCase();
+        } else if (FormatUtils.isExtensionUri(typeURI)) {
+            String ext = typeURI.toString().replace(FormatUtils.EXT_URI_PREFIX,
+                    "");
+            this.extensions = new HashSet<String>();
+            this.extensions.add(ext);
+            this.summary = ext.toUpperCase();
+        }
+    }
+
+    /**
+     * @return the typeURI
+     */
+    public URI getTypeURI() {
+        return typeURI;
+    }
+
+    /**
+     * @param typeURI the typeURI to set
+     */
+    public void setTypeURI(URI typeURI) {
+        this.typeURI = typeURI;
+    }
+
+    /**
+     * @return the aliases
+     */
+    public Set<URI> getAliases() {
+        return aliases;
+    }
+
+    /**
+     * @param aliases the aliases to set
+     */
+    public void setAliases(Set<URI> aliases) {
+        this.aliases = aliases;
+    }
+
+    /**
+     * @return the registryURL
+     */
+    public URL getRegistryURL() {
+        if (registryURL == null) {
+            try {
+                return typeURI.toURL();
+            } catch (MalformedURLException e) {
+                return null;
+            }
+        }
+        return registryURL;
+    }
+
+    /**
+     * @param registryURL the registryURL to set
+     */
+    public void setRegistryURL(URL registryURL) {
+        this.registryURL = registryURL;
+    }
+
+    /**
+     * @return the summary
+     */
+    public String getSummary() {
+        return summary;
+    }
+
+    /**
+     * @return the summary with the version number appended.
+     */
+    public String getSummaryAndVersion() {
+        if (version != null)
+            return summary + " " + version;
+        return summary;
+    }
+
+    /**
+     * @param summary the summary to set
+     */
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    /**
+     * @return the version
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version the version to set
+     */
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    /**
+     * @return the mimeTypes
+     */
+    public Set<String> getMimeTypes() {
+        return mimeTypes;
+    }
+
+    /**
+     * @param mimeTypes the mimeTypes to set
+     */
+    public void setMimeTypes(Set<String> mimeTypes) {
+        this.mimeTypes = mimeTypes;
+    }
+
+    /**
+     * @return the extensions
+     */
+    public Set<String> getExtensions() {
+        return extensions;
+    }
+
+    /**
+     * @param extensions the extensions to set
+     */
+    public void setExtensions(Set<String> extensions) {
+        this.extensions = extensions;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Format) {
+            Format f = (Format) obj;
+            /*
+             * Format objects MUST be uniquely identified by the assigned URI.
+             */
+            return this.typeURI.equals(f.typeURI);
+        } else {
+            return super.equals(obj);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return this.typeURI.hashCode();
+    }
+}
