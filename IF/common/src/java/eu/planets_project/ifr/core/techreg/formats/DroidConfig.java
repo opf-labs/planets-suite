f66a3d3fab3fa4a2a913456c37c2b0c053031aff
carlwilson-bl <carl.wilson@bl.uk>
2009-12-07 11:53:30 +0000

    Altered to make use of the new service config utility.

--- IF/common/src/java/eu/planets_project/ifr/core/techreg/formats/DroidConfig.java
+++ IF/common/src/java/eu/planets_project/ifr/core/techreg/formats/DroidConfig.java
@@ -3,7 +3,11 @@
  */
 package eu.planets_project.ifr.core.techreg.formats;
 
+import java.io.File;
 import java.util.logging.Logger;
+
+import eu.planets_project.ifr.core.common.conf.Configuration;
+import eu.planets_project.ifr.core.common.conf.ServiceConfig;
 
 /**
  * Droid configuration settings.
@@ -11,26 +15,26 @@ import java.util.logging.Logger;
  *
  */
 class DroidConfig {
+	/** The logger */
     private static Logger log = Logger.getLogger(DroidConfig.class.getName());
-    
-    /***/
-    static final String LOCAL = "PC/droid/src/resources/";
-    /***/
-    static final String SIG = "DROID_SignatureFile_Planets.xml";
-    /***/
-    static final String CONF = "/server/default/data/";
-    /***/
-    static final String JBOSS_HOME_DIR_KEY = "jboss.home.dir";
 
+    /** Properties keys for DROID sig file information */
+    private static final String COMMON_CONF_FILE_NAME = "Common";
+    private static final String SIG_FILE_LOC_KEY = "droid.sigfile.location";
+    private static final String SIG_FILE_NAME_KEY = "droid.sigfile.name";
     /**
-     * @return The DROID signature file location. If running in JBoss, from the deployment directory, else (like when
-     *         running a unit test) from the project directory
+     * @return The location of the DROID signature file taken from the
+     * 		   Droid configuration properties file
      */
-    static String signatureFileLocation() {
-        String deployedJBossHome = System.getProperty(JBOSS_HOME_DIR_KEY);
-        String sigFileFolder = (deployedJBossHome != null ? deployedJBossHome + CONF : LOCAL);
-        String sigFileLocation = sigFileFolder + SIG;
-        log.info("Opening signature file: " + sigFileLocation);
+    public static String configFolder() {
+    	// String to hold the location
+        String sigFileLocation = null;
+        // Get the configuration object from the ServiceConfig util
+        Configuration conf = ServiceConfig.getConfiguration(COMMON_CONF_FILE_NAME);
+        // Create the file name from the properties
+        sigFileLocation = conf.getString(SIG_FILE_LOC_KEY) +
+        		File.separator + conf.getString(SIG_FILE_NAME_KEY);
+        log.info("DROID Signature File location:" + sigFileLocation);
         return sigFileLocation;
     }
 
