7fb0c8eb20ed8ee3d22dd3d9c87fe9d7f5d98d3d
Thomas Skou Hansen <tsh@statsbiblioteket.dk>
2009-12-22 15:17:16 +0000

    - Added property handling to the service description parser. - Enhanced the JUnit test for ServiceDescriptionFactory. - Improved the example configuration file for the generic wrapper: GenericWrapperConfigExample.xml

--- IF/generic/src/main/java/eu/planets_project/ifr/core/services/migration/genericwrapper2/ServiceDescriptionFactory.java
+++ IF/generic/src/main/java/eu/planets_project/ifr/core/services/migration/genericwrapper2/ServiceDescriptionFactory.java
@@ -6,20 +6,30 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import eu.planets_project.ifr.core.services.migration.genericwrapper2.exceptions.ConfigurationException;
 import eu.planets_project.ifr.core.services.migration.genericwrapper2.exceptions.MigrationPathConfigException;
 import eu.planets_project.services.datatypes.Parameter;
+import eu.planets_project.services.datatypes.Property;
 import eu.planets_project.services.datatypes.ServiceDescription;
 import eu.planets_project.services.datatypes.Tool;
+import eu.planets_project.services.datatypes.Property.Builder;
 
 /**
  * @author Pelle Kofod &lt;pko@statsbiblioteket.dk&gt;
@@ -29,11 +39,14 @@ class ServiceDescriptionFactory {
     private Logger log = Logger.getLogger(ServiceDescriptionFactory.class
 	    .getName());
 
+    private final XPathFactory xPathFactory;
+
     private final Document configuration;
     private final String canonicalServiceName;
 
     ServiceDescriptionFactory(String canonicalServiceName,
 	    Document wrapperConfiguration) {
+	xPathFactory = XPathFactory.newInstance();
 	configuration = wrapperConfiguration;
 	this.canonicalServiceName = canonicalServiceName;
     }
@@ -41,97 +54,270 @@ class ServiceDescriptionFactory {
     ServiceDescription getServiceDescription() throws ConfigurationException,
 	    MigrationPathConfigException {
 
-	NodeList topLevelNodes = configuration.getElementsByTagName(
-		Constants.SERVICE_DESCRIPTION).item(0).getChildNodes();
+	try {
 
-	String title = null, description = null, version = null, creator = null, publisher = null, identifier = null, instructions = null, furtherinfo = null, logo = null;
+	    final XPath pathsXPath = xPathFactory.newXPath();
+	    final Node serviceDescriptionNode = (Node) pathsXPath.evaluate(
+		    "//serviceWrapping/serviceDescription", configuration,
+		    XPathConstants.NODE);
 
-	Tool tool = null;
+	    NodeList serviceDescriptionElements = configuration
+		    .getElementsByTagName(Constants.SERVICE_DESCRIPTION)
+		    .item(0).getChildNodes();
 
-	for (int nodeIndex = 0; nodeIndex < topLevelNodes.getLength(); nodeIndex++) {
-	    final Node currentNode = topLevelNodes.item(nodeIndex);
-	    if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
-		if (currentNode.getNodeName().equals(Constants.TITLE)) {
-		    title = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(
-			Constants.DESCRIPTION)) {
-		    description = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(Constants.TOOL)) {
-		    tool = parseTool(currentNode);
-		} else if (currentNode.getNodeName().equals(Constants.VERSION)) {
-		    version = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(Constants.CREATOR)) {
-		    creator = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName()
-			.equals(Constants.PUBLISHER)) {
-		    publisher = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(
-			Constants.IDENTIFIER)) {
-		    identifier = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(
-			Constants.INSTRUCTIONS)) {
-		    instructions = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(
-			Constants.FURTHERINFO)) {
-		    furtherinfo = currentNode.getTextContent().trim();
-		} else if (currentNode.getNodeName().equals(Constants.LOGO)) {
-		    logo = currentNode.getTextContent().trim();
+	    String title = null, description = null, version = null, creator = null, publisher = null, identifier = null, instructions = null, furtherinfo = null, logo = null;
+
+	    Tool toolDescription = null;
+
+	    for (int nodeIndex = 0; nodeIndex < serviceDescriptionElements
+		    .getLength(); nodeIndex++) {
+		final Node currentNode = serviceDescriptionElements
+			.item(nodeIndex);
+		if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+		    if (currentNode.getNodeName().equals(Constants.TITLE)) {
+			title = currentNode.getTextContent().trim();
+		    }/*
+		      * FIXME! KILL else if (currentNode.getNodeName().equals(
+		      * Constants.DESCRIPTION)) { description =
+		      * currentNode.getTextContent().trim(); }
+		      */else if (currentNode.getNodeName().equals(
+			    Constants.TOOL)) {
+			toolDescription = parseTool(currentNode);
+		    } else if (currentNode.getNodeName().equals(
+			    Constants.VERSION)) {
+			version = currentNode.getTextContent().trim();
+		    } else if (currentNode.getNodeName().equals(
+			    Constants.CREATOR)) {
+			creator = currentNode.getTextContent().trim();
+		    } else if (currentNode.getNodeName().equals(
+			    Constants.IDENTIFIER)) {
+			identifier = currentNode.getTextContent().trim();
+		    } else if (currentNode.getNodeName().equals(
+			    Constants.INSTRUCTIONS)) {
+			instructions = currentNode.getTextContent().trim();
+		    } else if (currentNode.getNodeName().equals(
+			    Constants.FURTHERINFO)) {
+			furtherinfo = currentNode.getTextContent().trim();
+		    } else if (currentNode.getNodeName().equals(Constants.LOGO)) {
+			logo = currentNode.getTextContent().trim();
+		    }
 		}
 	    }
-	}
-	if (title == null) {
-	    throw new ConfigurationException("title not set in configfile");
-	}
-	if (creator == null) {
-	    throw new ConfigurationException("creator not set in configfile");
-	}
-	
-	//FIXME! Is that the correct type passed on to the builder? Shouldn't it be the type of the concrete service?
-	ServiceDescription.Builder builder = new ServiceDescription.Builder(
-		title, "eu.planets_project.ifr.services.migrate.Migrate");
 
-	try {
+	    description = getDescription(serviceDescriptionNode);
+
+	    Property[] serviceProperties = getServiceProperties(serviceDescriptionNode);
+	    // TODO: Check that all mandatory fields have been properly filled
+	    // out.
+	    if (title == null) {
+		throw new ConfigurationException("title not set in configfile");
+	    }
+	    if (creator == null) {
+		throw new ConfigurationException(
+			"creator not set in configfile");
+	    }
+
+	    // FIXME! Is that the correct type passed on to the builder?
+	    // Shouldn't
+	    // it be the type of the concrete service?
+	    ServiceDescription.Builder builder = new ServiceDescription.Builder(
+		    title, "eu.planets_project.ifr.services.migrate.Migrate");
+
 	    builder.author(creator);
 	    builder.classname(canonicalServiceName);
+	    // FIXME! I geuss that the end-point URL should be obtained
+	    // dynamically to ensure that it is in sync. with the actual
+	    // end-point URL.
 	    builder
 		    .endpoint(new URL("http://FIXME! put the correct URL here!"));
 	    builder.description(description);
 	    builder.identifier(identifier);
 	    builder.instructions(instructions);
 	    builder.version(version);
-	    builder.tool(tool);
-	    builder.serviceProvider(publisher);
+	    builder.tool(toolDescription);
+
+	    // TODO: Publisher is dynamic and depends on the installation site.
+	    // builder.serviceProvider(publisher);
 	    eu.planets_project.services.datatypes.MigrationPath planetsPaths[] = getPlanetsMigrationPaths(configuration);
 	    builder.paths(planetsPaths);
 	    builder.inputFormats(getInputFormats(planetsPaths));
-	    
-	    //FIXME! Collect all unique parameters from all the paths defined in the configuration.
-	    final List<Parameter> parameters = planetsPaths[0].getParameters();
+
+	    final List<Parameter> parameters = getUniqueParameters(planetsPaths);
 	    builder.parameters(parameters);
-	} catch (MalformedURLException mue) {
+
+	    builder.properties(serviceProperties);
+	    if (furtherinfo != null) {
+		try {
+		    builder.furtherInfo(new URI(furtherinfo));
+		} catch (URISyntaxException e) {
+		    throw new ConfigurationException(
+			    "furtherInfo not set to valid value", e);
+		}
+	    }
+	    if (logo != null) {
+		try {
+		    builder.logo(new URI(logo));
+		} catch (URISyntaxException e) {
+		    throw new ConfigurationException(
+			    "logo not set to valid value", e);
+		}
+	    }
+
+	    return builder.build();
+	} catch (Exception exception) {
 	    throw new ConfigurationException(
-		    "Failed adding end-point information to the service "
-			    + "description.", mue);
+		    "Failed parsing the service description element of the "
+			    + "generic wrapper configuration.", exception);
 	}
-	if (furtherinfo != null) {
-	    try {
-		builder.furtherInfo(new URI(furtherinfo));
-	    } catch (URISyntaxException e) {
-		throw new ConfigurationException(
-			"furtherInfo not set to valid value", e);
+
+    }
+
+    private Property[] getServiceProperties(Node serviceDescriptionNode)
+	    throws ConfigurationException {
+
+	Node propertyNode = null;
+	try {
+	    final XPath pathsXPath = xPathFactory.newXPath();
+	    final NodeList propertyNodes = (NodeList) pathsXPath.evaluate(
+		    "properties/property", serviceDescriptionNode,
+		    XPathConstants.NODESET);
+
+	    ArrayList<Property> properties = new ArrayList<Property>();
+	    for (int propertyIdx = 0; propertyIdx < propertyNodes.getLength(); propertyIdx++) {
+		propertyNode = propertyNodes.item(propertyIdx);
+
+		final NamedNodeMap propertyAttributes = propertyNode
+			.getAttributes();
+
+		// Get the values for the mandatory attributes and sub-nodes.
+		final URI propertyID = new URI(propertyAttributes.getNamedItem(
+			"id").getNodeValue());
+		Property.Builder propertyBuilder = new Property.Builder(
+			propertyID);
+		propertyBuilder.name(propertyAttributes.getNamedItem("name")
+			.getNodeValue());
+
+		propertyBuilder = addValue(propertyBuilder, propertyNode);
+
+		// Add values from optional attributes and sub-nodes.
+		try {
+		    propertyBuilder.type(propertyAttributes
+			    .getNamedItem("type").getNodeValue());
+
+		    propertyBuilder.description(getDescription(propertyNode));
+		} catch (Exception exception) {
+		    // Ignore exceptions thrown due to missing optional
+		    // attributes and sub-nodes.
+		}
+
+		properties.add(propertyBuilder.build());
 	    }
+	    return properties.toArray(new Property[properties.size()]);
+	} catch (Exception exception) {
+	    throw new ConfigurationException(String.format(
+		    "Failed parsing the '%s' element of the configuration"
+			    + " document.", serviceDescriptionNode
+			    .getNodeName()), exception);
 	}
-	if (logo != null) {
-	    try {
-		builder.logo(new URI(logo));
-	    } catch (URISyntaxException e) {
-		throw new ConfigurationException("logo not set to valid value",
-			e);
+    }
+
+    /**
+     * Add the text content of <code>&lt;value&gt;</code> in
+     * <code>nodeWithValueElement</code> and the value of its optional
+     * <code>&quot;unit&quot;</code> attribute to the
+     * <code>propertyBuilder</code>.
+     * 
+     * @param propertyBuilder
+     *            <code>Property.Builder</code> instance to store the retrieved
+     *            data in.
+     * @param nodeWithValueElement
+     *            <code>Node</code> instance having a <code>&lt;value&gt;</code>
+     *            sub-node.
+     * @return the <code>Property.Builder</code> specified by
+     *         <code>propertyBuilder</code> with the added data retrieved from
+     *         <code>nodeWithValueElement</code>.
+     * @throws XPathExpressionException
+     *             if no <code>&lt;value&gt;</code> element is found in
+     *             <code>nodeWithValueElement</code>.
+     */
+    private Builder addValue(Property.Builder propertyBuilder,
+	    Node nodeWithValueElement) throws XPathExpressionException {
+	final XPath pathsXPath = xPathFactory.newXPath();
+	final Node valueNode = (Node) pathsXPath.evaluate("value",
+		nodeWithValueElement, XPathConstants.NODE);
+
+	propertyBuilder.value(valueNode.getTextContent().trim());
+
+	NamedNodeMap valueAttributes = valueNode.getAttributes();
+	propertyBuilder.unit(valueAttributes.getNamedItem("unit")
+		.getNodeValue());
+
+	return propertyBuilder;
+    }
+
+    /**
+     * Return the text content of the <code>&lt;description&gt;</code> element
+     * of <code>nodeWithDescriptionElement</code>.
+     * 
+     * @param nodeWithDescriptionElement
+     *            <code>Node</code> containing a
+     *            <code>&lt;description&gt;</code> (child) element.
+     * @return a <code>String</code> instance containing the text content of the
+     *         description element.
+     * @throws XPathExpressionException
+     *             if no <code>&lt;description&gt;</code> element is found in
+     *             <code>nodeWithDescriptionElement</code>.
+     */
+    private String getDescription(Node nodeWithDescriptionElement)
+	    throws XPathExpressionException {
+	final XPath pathsXPath = xPathFactory.newXPath();
+	final Node descriptionNode = (Node) pathsXPath.evaluate("description",
+		nodeWithDescriptionElement, XPathConstants.NODE);
+
+	return descriptionNode.getTextContent().trim();
+    }
+
+    /**
+     * Collect all the unique parameters (unique by name) defined for the
+     * migration paths specified by <code>planetsPaths</code> and return them in
+     * a list.
+     * <p/>
+     * <p/>
+     * The same parameter is likely to be defined for multiple paths, however,
+     * if the configuration file for the generic wrapper has been properly
+     * written, then all occurrences should have the same meaning, thus, only
+     * the first occurrence of a parameter will be collected. For the same
+     * reason, the returned parameters will not have any value to avoid
+     * confusion, as the default value of a parameter may differ among the
+     * various migration paths.
+     * 
+     * @param planetsPaths
+     *            An array of <code>MigrationPath</code> instances to collect
+     *            parameters from.
+     * @return <code>List</code> of unique parameters declared for the paths
+     *         specified by <code>planetsPaths</code>. The value of all the
+     *         parameters will be <code>null</code>.
+     */
+    private List<Parameter> getUniqueParameters(
+	    eu.planets_project.services.datatypes.MigrationPath[] planetsPaths) {
+
+	final HashMap<String, Parameter> parameterMap = new HashMap<String, Parameter>();
+	for (eu.planets_project.services.datatypes.MigrationPath migrationPath : planetsPaths) {
+	    for (Parameter parameter : migrationPath.getParameters()) {
+
+		// Collect the parameter and ignore multiple occurrences.
+		// Parameters having a value will have the value stripped.
+		final String parameterName = parameter.getName();
+		if (!parameterMap.containsKey(parameterName)) {
+
+		    Parameter.Builder parameterBuilder = new Parameter.Builder(
+			    parameterName, null);
+		    parameterMap.put(parameterName, parameterBuilder.build());
+		}
 	    }
 	}
 
-	return builder.build();
-
+	return new ArrayList<Parameter>(parameterMap.values());
     }
 
     /**
@@ -158,7 +344,7 @@ class ServiceDescriptionFactory {
 
 	final DBMigrationPathFactory migrationPathFactory = new DBMigrationPathFactory(
 		wrapperConfiguration);
-	
+
 	final MigrationPaths migrationPaths = migrationPathFactory
 		.getAllMigrationPaths();
 
@@ -194,6 +380,12 @@ class ServiceDescriptionFactory {
 	    }
 
 	}
+	try {
+	    description = getDescription(tool);
+	} catch (XPathExpressionException xpee) {
+	    // The description is optional, thus ignore exceptions.
+	}
+
 	URL homepageURL = null;
 	URI identifierURI = null;
 	if (homepage != null) {
@@ -248,8 +440,18 @@ class ServiceDescriptionFactory {
 	    final Collection<Preset> presets = toolPresets.getAllToolPresets();
 	    for (Preset preset : presets) {
 
-		Parameter.Builder parameterBuilder = new Parameter.Builder(
-			preset.getName(), null);
+		// Create a parameter for each preset which is not assigned any
+		// value (i.e. null), however, the default preset parameter will
+		// be
+		// assigned the name of the default preset setting.
+		Parameter.Builder parameterBuilder;
+		if (preset.getName().equals(toolPresets.getDefaultPresetID())) {
+		    parameterBuilder = new Parameter.Builder(preset.getName(),
+			    preset.getDefaultSetting().getName());
+		} else {
+		    parameterBuilder = new Parameter.Builder(preset.getName(),
+			    null);
+		}
 
 		// Append a description of the valid values for the preset
 		// parameter.
--- IF/generic/test/java/eu/planets_project/ifr/core/services/migration/genericwrapper2/ServiceDescriptionFactoryTest.java
+++ IF/generic/test/java/eu/planets_project/ifr/core/services/migration/genericwrapper2/ServiceDescriptionFactoryTest.java
@@ -5,8 +5,12 @@ package eu.planets_project.ifr.core.services.migration.genericwrapper2;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,7 +20,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import eu.planets_project.ifr.core.services.migration.genericwrapper2.utils.DocumentLocator;
+import eu.planets_project.services.datatypes.Parameter;
+import eu.planets_project.services.datatypes.Property;
 import eu.planets_project.services.datatypes.ServiceDescription;
+import eu.planets_project.services.datatypes.Tool;
 
 /**
  * @author Thomas Skou Hansen &lt;tsh@statsbiblioteket.dk&gt;
@@ -68,9 +75,6 @@ public class ServiceDescriptionFactoryTest {
 	assertEquals("Un-expected description.", expectedDescription,
 		serviceDescription.getDescription());
 
-	// assertEquals("Un-expected end-point.","",
-	// serviceDescription.getEndpoint());
-
 	final URI expectedFurtherInfoURI = new URI("http://example.org");
 	assertEquals("Un-expected text returned by getFurtherInfo().",
 		expectedFurtherInfoURI, serviceDescription.getFurtherInfo());
@@ -85,7 +89,7 @@ public class ServiceDescriptionFactoryTest {
 	expectedInputFormats.add(new URI("info:pronom/x-fmt/406"));
 	expectedInputFormats.add(new URI("info:planets/fmt/ext/foo"));
 	expectedInputFormats.add(new URI("info:pronom/x-fmt/91"));
-	
+
 	assertEquals("Un-expected input formats.", expectedInputFormats,
 		new HashSet<URI>(serviceDescription.getInputFormats()));
 
@@ -102,24 +106,110 @@ public class ServiceDescriptionFactoryTest {
 		"Example: Eggnog migration service.", serviceDescription
 			.getName());
 
-	/* TODO: Yet to be finished....
-	System.out.println("@%^@^%@%^ GNARF>>> "
-		+ serviceDescription.getParameters());
+	verifyParameters(serviceDescription.getParameters());
 
-	assertEquals("Un-expected parameter list.", "", serviceDescription
-		.getParameters());
-	assertEquals("Un-expected property list.", "", serviceDescription
-		.getProperties());
-	assertEquals("Un-expected service provider information.", "",
-		serviceDescription.getServiceProvider());
-	assertEquals("Un-expected tool description.", "", serviceDescription
-		.getTool());
-
-	assertEquals("Un-expected service interface type.", "",
-		serviceDescription.getType());
-		*/
+	verifyToolDescription(serviceDescription.getTool());
 
 	assertEquals("Un-expected service version.", "3.141592653589793",
 		serviceDescription.getVersion());
+
+	assertEquals("Un-expected end-point URL.", new URL(
+		"http://FIXME! put the correct URL here!"), serviceDescription
+		.getEndpoint());
+
+	verifyProperties(serviceDescription.getProperties());
+	// TODO: Yet to be finished.... Clarify where this information is
+	// supposed to come from...
+	//	  
+	// assertEquals("Un-expected service provider information.", "",
+	// serviceDescription.getServiceProvider());
+	//	  
+	// assertEquals("Un-expected service interface type.", "",
+	// serviceDescription.getType());
+
+	System.out
+		.println("@%^@^%@%^ GNARF>>> " + serviceDescription.getTool());
+
+    }
+
+    private void verifyProperties(List<Property> properties) throws Exception {
+
+	assertEquals(
+		"Un-expected number of properties in service description.", 1,
+		properties.size());
+
+	final Property property = properties.get(0);
+	final String expectedDescription = "This is an optional description of"
+		+ " an example property with a value\n\t\t\t\t\tand a silly value "
+		+ "unit specification.";
+	assertEquals("Un-expected property description.", expectedDescription,
+		property.getDescription());
+	assertEquals("Un-expected property name.", "exampleName", property
+		.getName());
+	assertEquals("Un-expected property type.", "myOptionalExampleType",
+		property.getType());
+	assertEquals("Un-expected property ID.", new URI("info:exampleID"),
+		property.getUri());
+	assertEquals("Un-expected property value unit.", "cubic gram", property
+		.getUnit());
+	assertEquals("Un-expected ", "exampleValue with a ridiculous unit.",
+		property.getValue());
+    }
+
+    private void verifyToolDescription(Tool toolDescription)
+	    throws URISyntaxException, MalformedURLException {
+
+	final URI id = new URI(
+		"http://example-planets-registry.eu/toolident?4385794357");
+	final String name = "Example: HandMixer.exe";
+	final String version = "HandMixer V4.13 by J. Random Hacker.";
+	final String description = "Example: A useful tool for migrating eggs "
+		+ "to eggnog.";
+	final URL homeURL = new URL("http://example.org");
+
+	final Tool expectedToolDescription = new Tool(id, name, version,
+		description, homeURL);
+
+	assertEquals("Un-expected URI in tool description.",
+		expectedToolDescription.getIdentifier(), toolDescription
+			.getIdentifier());
+	assertEquals("Un-expected tool name in tool description.",
+		expectedToolDescription.getName(), toolDescription.getName());
+	assertEquals("Un-expected tool version in tool description.",
+		expectedToolDescription.getVersion(), toolDescription
+			.getVersion());
+	assertEquals("Un-expected tool description .", expectedToolDescription
+		.getDescription(), toolDescription.getDescription());
+	assertEquals("Un-expected tool homepage URL in tool description.",
+		expectedToolDescription.getHomepage(), toolDescription
+			.getHomepage());
+    }
+
+    /**
+     * Verify that the parameters specified by <code>parameters</code> have the
+     * expected names and values. If that is not the case then JUnit assertions
+     * will be triggered.
+     * 
+     * @param parameters
+     *            <code>List</code> of parameters to verify.
+     */
+    private void verifyParameters(List<Parameter> parameters) {
+
+	final HashMap<String, String> expectedParameters = new HashMap<String, String>();
+	expectedParameters.put("param1", null);
+	expectedParameters.put("param2", null);
+	expectedParameters.put("mode", null);
+	expectedParameters.put("quality", null);
+
+	for (Parameter parameter : parameters) {
+
+	    assertTrue("Un-expected parameter in the parameters list of the "
+		    + "service description: " + parameter.getName(),
+		    expectedParameters.containsKey(parameter.getName()));
+
+	    assertEquals("Un-expected value for parameter '"
+		    + parameter.getName() + "'.", expectedParameters
+		    .get(parameter.getName()), parameter.getValue());
+	}
     }
 }
