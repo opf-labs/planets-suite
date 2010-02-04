14f12e3e8a1f0228fc61a5940ffd29878bb5e7f0
carlwilson-bl <carl.wilson@bl.uk>
2010-02-04 17:09:43 +0000

    Adding classes for calling the DLS Get web service.

---
+++ IF/storage/src/main/java/eu/planets_project/ifr/core/storage/impl/bl/dls/uitls/GetMessageBody.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package eu.planets_project.ifr.core.storage.impl.bl.dls.uitls;
+
+import org.apache.soap.Body;
+
+/**
+ * @author carl
+ *
+ */
+public class GetMessageBody extends Body {
+
+}
---
+++ IF/storage/src/main/java/eu/planets_project/ifr/core/storage/impl/bl/dls/uitls/GetServiceProxy.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package eu.planets_project.ifr.core.storage.impl.bl.dls.uitls;
+
+/**
+ * @author carl
+ *
+ */
+public class GetServiceProxy {
+
+}
