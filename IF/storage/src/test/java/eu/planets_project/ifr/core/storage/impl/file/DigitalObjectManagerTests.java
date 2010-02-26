ce108f0af5821690a3ee6a342cfd6c603c93d20b
carlwilson-bl <carl.wilson@bl.uk>
2010-02-26 13:51:25 +0000

    Added lots of nice output to try to work out why the tests fail on Hudson.

--- IF/storage/src/test/java/eu/planets_project/ifr/core/storage/impl/file/DigitalObjectManagerTests.java
+++ IF/storage/src/test/java/eu/planets_project/ifr/core/storage/impl/file/DigitalObjectManagerTests.java
@@ -41,7 +41,7 @@ public class DigitalObjectManagerTests {
     private static final String MISSING_NAME_PROPS = "missingname.properties";
     private static final String BAD_NAME_PROPS = "badname.properties";
     private static final String MISSING_PATH_PROPS = "missingpath.properties";
-    private DigitalObjectManagerBase _dom = null;
+    private DigitalObjectManagerBase dom = null;
 	
 	/**
 	 * @throws java.lang.Exception
@@ -57,7 +57,7 @@ public class DigitalObjectManagerTests {
         // Instantiate a file based data registry instance
 		// Point it at a root directory in resources, the registry will create the dir if necessary
         Configuration config = ServiceConfig.getConfiguration(new File(CONFIG + TEMP_PROPS));
-		_dom = new TempFilesystemDigitalObjectManagerImpl(config);
+		this.dom = new TempFilesystemDigitalObjectManagerImpl(config);
 	}
 
 	/**
@@ -67,10 +67,10 @@ public class DigitalObjectManagerTests {
 	@Test
 	public final void testList() throws URISyntaxException {
 		// Get the root URI
-		List<URI> rootResults = _dom.list(null);
+		List<URI> rootResults = this.dom.list(null);
 		System.out.println("Performing the null URI test to obtain root URI");
 		List<URI> expectedResults = new ArrayList<URI>();
-		expectedResults.add(DataRegistryFactory.createDataRegistryIdFromName(_dom.getName()));
+		expectedResults.add(DataRegistryFactory.createDataRegistryIdFromName(this.dom.getName()));
 		// We should only have a single URI in the returned results
 		assertEquals("Too many results returned, expecting one and got " + rootResults.size(),
 				expectedResults.size(),	rootResults.size());
@@ -80,7 +80,7 @@ public class DigitalObjectManagerTests {
 	}
 
 	/**
-	 * Test method for {@link eu.planets_project.ifr.core.storage.impl.file.FilesystemDigitalObjectManagerImpl#store(java.net.URI, eu.planets_project.services.datatypes.DigitalObject)}.
+	 * Test method for {@link eu.planets_project.ifr.core.storage.impl.file.FilesystemDigitalObjectManagerImpl#storeAsNew(java.net.URI, eu.planets_project.services.datatypes.DigitalObject)}.
 	 * @throws URISyntaxException 
 	 * @throws DigitalObjectNotStoredException 
 	 * @throws DigitalObjectNotFoundException 
@@ -92,44 +92,71 @@ public class DigitalObjectManagerTests {
 		System.out.println("Testing storage of Digital Object");
 		URI purl = new File(AllStorageSuite.TEST_DATA_BASE, FILE).toURI();
         /* Create the content: */
+		System.out.println("Creating DigitalObjectContent c1");
         DigitalObjectContent c1 = Content.byReference(purl.toURL().openStream());
+		System.out.println("Creating DigitalObject object");
         /* Given these, we can instantiate our object: */
         DigitalObject object = new DigitalObject.Builder(c1).permanentUri(purl).title(purl.toString()).build();
     	// Check digital object. Title should not be null 
+		System.out.println("Setting store flag to true");
         boolean storeFlag = true;
         // Now store it
         URI pdURI = null;
         try {
-
-            pdURI = _dom.storeAsNew(object);
+    		System.out.println("calling this.dom.storeAsNew(object)");
+            pdURI = this.dom.storeAsNew(object);
         } catch (Exception e) {
-        	assertTrue(e.getClass().equals(DigitalObjectNotStoredException.class));
+    		System.out.println("Caught an exception in storeAsNew, here's the details");
+        	e.printStackTrace();
+    		System.out.println("Asserting it's a not stored exception");
+        	assertTrue("Expecting exception to be DigitalObjectNotStoredException", e.getClass().equals(DigitalObjectNotStoredException.class));
+    		System.out.println("Setting storeFlag to false");
+        	storeFlag = false;
+        } catch (Throwable t) {
+    		System.out.println("Caught a throwable in storeAsNew, here's the details");
+        	t.printStackTrace();
+    		System.out.println("Setting storeFlag to false");
         	storeFlag = false;
         }
         
+		System.out.println("Creating new version of object");
         object = new DigitalObject.Builder(object.getContent()).title("mytitle").build();
-        assertNotNull(object.getTitle());
+		System.out.println("asserting the title is not null");
+        assertNotNull("NOT expecting object.getTitle() to be null", object.getTitle());
         
         if (storeFlag)
         {
 			// Then retrieve it and check it's the same
-			DigitalObject retObject = _dom.retrieve(pdURI);
+    		System.out.println("now retrieving object to test (ret object)");
+			DigitalObject retObject = this.dom.retrieve(pdURI);
+			System.out.println("Creating new Purl");
 			URI newPurl = new File(AllStorageSuite.TEST_DATA_BASE, FILE).toURI();
+			System.out.println("Creating digital object c2");
 			DigitalObjectContent c2 = Content.byReference(newPurl.toURL().openStream());
+			System.out.println("Creating new Expected object");
 			DigitalObject expectedObject = new DigitalObject.Builder(c2).build(); 
 			
-            assertEquals("Retrieve Digital Object content ("+expectedObject.getContent()+") doesn't match that stored ("+retObject.getContent()+")", expectedObject.getContent(),
+			System.out.println("Trying content match between expectedObject and retobject");
+            assertEquals("Retrieve Digital Object content (" +
+            		expectedObject.getContent() +
+            		") doesn't match that stored (" + retObject.getContent() +
+            		")", expectedObject.getContent(),
                     retObject.getContent());
 			// We can test that the list method works properly now also
 			// Get the root URI
-			List<URI> rootResults = _dom.list(null);
+			System.out.println("trying list out");
+			List<URI> rootResults = this.dom.list(null);
+			System.out.println("getting expectedResults");
 			List<URI> expectedResults = new ArrayList<URI>();
+			System.out.println("adding entry to expectedResults");
 			expectedResults.add(new URI("planets://localhost:8080/dr/test/" + FILE));
 			// We should only have a single URI in the returned results
+			System.out.println("testing count");
 			assertEquals("Original and retrieved result count should be equal;",
 					expectedResults.size(),	rootResults.size());
 			// We have the root so let's get what's below
-			List<URI> testResults = _dom.list(rootResults.get(0));
+			System.out.println("getting testResults from list");
+			List<URI> testResults = this.dom.list(rootResults.get(0));
 			// We should only have a single URI in the returned results
 			assertEquals("Original and retrieved result count should be equal;",
 					expectedResults.size(),	testResults.size());
@@ -151,7 +178,7 @@ public class DigitalObjectManagerTests {
 	public final void testFileNotFound() throws URISyntaxException, DigitalObjectNotFoundException {
 			System.out.println("Testing that DigitalObjectNotFoundException is generated as expected");
 			// Let's retrieve an object we know doesn't exist
-			_dom.retrieve(new URI((this._dom.getId()).toString() + "/noneexistentobject").normalize());
+			this.dom.retrieve(new URI((this.dom.getId()).toString() + "/noneexistentobject").normalize());
 	}
 	
 	/**
