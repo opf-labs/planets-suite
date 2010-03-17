7d2a5d9cbcb376fb8611688f147b6258bbfa89b0
Roman Graf <roman.graf@ait.ac.at>
2010-03-17 09:14:25 +0000

    Performance for OAI registry improved.

--- IF/storage/src/main/java/eu/planets_project/ifr/core/storage/impl/oai/OAIDigitalObjectManagerDCImpl.java
+++ IF/storage/src/main/java/eu/planets_project/ifr/core/storage/impl/oai/OAIDigitalObjectManagerDCImpl.java
@@ -12,11 +12,19 @@ import se.kb.oai.pmh.Record;
 import se.kb.oai.OAIException;
 import se.kb.oai.pmh.OaiPmhServer;
 import eu.planets_project.ifr.core.storage.api.query.Query;
+//import eu.planets_project.ifr.core.storage.api.query.QueryDateRange;
 import eu.planets_project.ifr.core.storage.api.query.QueryValidationException;
 import eu.planets_project.services.datatypes.Content;
 import eu.planets_project.services.datatypes.DigitalObject;
 import eu.planets_project.services.datatypes.Metadata;
 import eu.planets_project.services.datatypes.DigitalObject.Builder;
+
+//import java.util.Calendar;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 /**
  * DC implementation of the OAI digital object manager.
@@ -24,10 +32,23 @@ import eu.planets_project.services.datatypes.DigitalObject.Builder;
 public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManagerImpl {
 	
 	/**
+	 * The cache map binds URI with the digital object.
+	 */
+	private static Map<URI, DigitalObject> leafMap = new HashMap<URI, DigitalObject>();
+	
+    /**
+     * The manager control thread.
+     */
+    private static ManagerControl mc;
+
+	/**
 	 * @param baseURL The base URL
 	 */
 	public OAIDigitalObjectManagerDCImpl(String baseURL) {
 		super(baseURL, OAIDigitalObjectManagerDCBase.PREFIX);
+        mc = new ManagerControl(this);
+        mc.start();
+        log.info("Manager control thread started.");
 	}
 	
 
@@ -54,6 +75,13 @@ public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManag
     	long starttime = System.currentTimeMillis();
     	log.info("OAIDigitalObjectManagerDCImpl retrieve() starttime: " + starttime);
 		try {
+			if (pdURI != null && leafMap.containsKey(pdURI)) {
+		    	log.info("OAIDigitalObjectManagerDCImpl retrieve() already exist in map uri: " + pdURI);
+		    	long endtime = System.currentTimeMillis();
+		    	log.info("OAIDigitalObjectManagerDCImpl retrieve() timediff: " + (endtime - starttime));
+				return leafMap.get(pdURI);
+			}
+
 			OaiPmhServer server = new OaiPmhServer(baseURL);
 			Record record = server.getRecord(pdURI.toString(), metaDataPrefix);
 			Element metadata = record.getMetadata();
@@ -103,17 +131,25 @@ public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManag
 					builder.metadata(new Metadata(namespaceURI, record.getMetadataAsString()));
 			    	long endtime = System.currentTimeMillis();
 			    	log.info("OAIDigitalObjectManagerDCImpl retrieve() timediff: " + (endtime - starttime));
-					return builder.build();
+					DigitalObject o = builder.build();
+					if (pdURI != null && !leafMap.containsKey(pdURI)) {
+				    	log.info("OAIDigitalObjectManagerDCImpl retrieve() add to map uri: " + pdURI);
+						leafMap.put(pdURI, o);
+					}
+					return o;
 				}
 			}
 			
-	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error: NoHTTP URL available.");
+	    	long endtime = System.currentTimeMillis();
+	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error1: NoHTTP URL available." + " timediff: " + (endtime - starttime));
 			throw new DigitalObjectNotFoundException("No HTTP URL available for this record");
 		} catch (OAIException e) {
-	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error: " + e.getMessage());
+	    	long endtime = System.currentTimeMillis();
+	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error2: " + e.getMessage() + " timediff: " + (endtime - starttime));
 			throw new DigitalObjectNotFoundException(e.getMessage());
 		} catch (IOException e) {
-	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error2: " + e.getMessage());
+	    	long endtime = System.currentTimeMillis();
+	    	log.info("OAIDigitalObjectManagerDCImpl retrieve() error3: " + e.getMessage() + " timediff: " + (endtime - starttime));
 			throw new DigitalObjectNotFoundException(e.getMessage());
 		}
 	}
@@ -136,7 +172,9 @@ public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManag
 		
 		// ListIdentifiers
 		System.out.println("starting query.");
-		List<URI> identifiers = oaiImpl.list(null);
+		try {
+//			List<URI> identifiers = oaiImpl.list(null, new QueryDateRange(start, now));	
+			List<URI> identifiers = oaiImpl.list(null);
 		System.out.println(identifiers.size() + " found.");
 		
 		// GetRecord for each identifier
@@ -148,6 +186,9 @@ public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManag
 				System.out.println("couldn't retrieve file: " + e.getMessage());
 			}
 		}
+		} catch (QueryValidationException e) {
+			System.out.println("QueryValidationException: " + e.getMessage());
+		}	
 		System.out.println("done.");
 	}
 	*/
@@ -164,5 +205,58 @@ public class OAIDigitalObjectManagerDCImpl extends AbstractOAIDigitalObjectManag
         throw new DigitalObjectNotStoredException("Storing not supported by this implementation.");
     }
 
-	
+    
+    /**
+     * This class manages the cache of retrieved digital objects.
+     * @author GrafR
+     *
+     */
+    public class ManagerControl extends Thread {
+        
+        /**
+         * Repository implementation
+         */
+        OAIDigitalObjectManagerDCImpl impl;
+        
+        /**
+         * The time between consistency check
+         */
+        long sleeptime = 1200000;
+
+        /**
+         * @param impl The repository implementation
+         */
+        public ManagerControl(OAIDigitalObjectManagerDCImpl _impl) {
+            this.impl = _impl;
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    log.info("ManagerControl run().");
+                    Thread.sleep(sleeptime);
+                    // check cache entries. Update digital object if necessary. 
+                    Iterator<URI> leafIterator = leafMap.keySet().iterator();
+                    while(leafIterator.hasNext()) {
+                        URI currentUri = leafIterator.next();
+                        log.info("ManagerControl check uri: " + currentUri);    
+                        try {
+                        	DigitalObject presentObj = impl.retrieve(currentUri);
+                        	if (!presentObj.equals(leafMap.get(currentUri))) {
+                        		leafMap.put(currentUri, presentObj);
+                        	}
+                        } catch (DigitalObjectNotFoundException e) {
+                            log.info("ManagerControl digital object not found for uri: " + currentUri);    
+                            // remove it if it is not more present in the repository.
+                            leafMap.remove(currentUri);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    log.info("ManagerControl error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
