337651f165c4deb5c62b9efc306eb72c1a7e159a
Fabian Steeg <fsteeg@gmail.com>
2010-04-19 12:24:28 +0000

    Tweaked ServiceRegistrySampleUsage to include a migration path

--- IF/servreg/src/test/java/eu/planets_project/ifr/core/servreg/ServiceRegistrySampleUsage.java
+++ IF/servreg/src/test/java/eu/planets_project/ifr/core/servreg/ServiceRegistrySampleUsage.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 
 import eu.planets_project.ifr.core.servreg.api.ServiceRegistry;
 import eu.planets_project.ifr.core.servreg.api.ServiceRegistryFactory;
+import eu.planets_project.ifr.core.techreg.formats.FormatRegistry;
+import eu.planets_project.ifr.core.techreg.formats.FormatRegistryFactory;
+import eu.planets_project.services.datatypes.MigrationPath;
 import eu.planets_project.services.datatypes.ServiceDescription;
 import eu.planets_project.services.identify.Identify;
 import eu.planets_project.services.migrate.Migrate;
@@ -27,6 +30,7 @@ public class ServiceRegistrySampleUsage {
     public void usage() throws MalformedURLException {
         /* We retrieve an instance of the registry: */
         ServiceRegistry registry = ServiceRegistryFactory.getServiceRegistry();
+        registry.clear(); // clear any old local entries
         URL endpoint1 = new URL("http://some.dummy.endpoint");
         URL endpoint2 = new URL("http://another.dummy.endpoint");
         /* We register service descriptions: */
@@ -34,15 +38,19 @@ public class ServiceRegistrySampleUsage {
                 .register(/* new Droid().describe() */new ServiceDescription.Builder(
                         "Droid", Identify.class.getName()).endpoint(endpoint1)
                         .build());
+        /* We can register supported migration paths with the service description: */
+        FormatRegistry formatRegistry = FormatRegistryFactory.getFormatRegistry();
+        MigrationPath path = new MigrationPath(formatRegistry.createExtensionUri("jpg"),
+                formatRegistry.createExtensionUri("bmp"), null);
         registry
                 .register(/* new SanselanMigrate().describe() */new ServiceDescription.Builder(
                         "Sanselan", Migrate.class.getName())
-                        .endpoint(endpoint2).build());
-        /* And can then query by example, e.g. for migration services: */
-        List<ServiceDescription> migrationServices = registry
-                .query(new ServiceDescription.Builder(null, Migrate.class
-                        .getName()).build());
-        /* Which we expect to return only the migration service: */
+                        .endpoint(endpoint2).paths(path).build());
+        /* And can then query by example, e.g. for migration services supporting the path: */
+        ServiceDescription example = new ServiceDescription.Builder(null, Migrate.class
+                .getName()).paths(path).build();
+        List<ServiceDescription> migrationServices = registry.query(example);
+        /* Which we expect to return only the compatible migration service: */
         Assert.assertEquals(1, migrationServices.size());
         Assert.assertEquals("Sanselan", migrationServices.get(0).getName());
         /* For further example on queries see CoreRegistryTests */
