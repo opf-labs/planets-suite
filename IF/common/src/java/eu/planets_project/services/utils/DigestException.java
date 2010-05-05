94c23260f15919fcf7b95d0c93c1c6bac0826c87
Andrew Jackson <Andrew.Jackson@bl.uk>
2010-05-05 11:40:12 +0000

    Fixed copyright statement at head of files.

--- IF/common/src/java/eu/planets_project/services/utils/DigestException.java
+++ IF/common/src/java/eu/planets_project/services/utils/DigestException.java
@@ -1,25 +1,13 @@
-/* $Id: DigestException.java,v 1.4 2007/12/04 13:22:01 mke Exp $
- * $Revision: 1.4 $
- * $Date: 2007/12/04 13:22:01 $
- * $Author: mke $
+/*******************************************************************************
+ * Copyright (c) 2007, 2010 The Planets Project Partners.
  *
- * The SB Util Library.
- * Copyright (C) 2005-2007  The State and University Library of Denmark
+ * All rights reserved. This program and the accompanying 
+ * materials are made available under the terms of the 
+ * Apache License, Version 2.0 which accompanies 
+ * this distribution, and is available at 
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+ *******************************************************************************/
 package eu.planets_project.services.utils;
 
 /**
--- IF/common/src/java/eu/planets_project/services/utils/ProcessRunner.java
+++ IF/common/src/java/eu/planets_project/services/utils/ProcessRunner.java
@@ -1,9 +1,13 @@
-/* 
- * ProcessRunner. 
- * Copyright (C) 2005-2008  The State and University Library
- * Added to the Planets Project by the State and University Library
- * Author Asger Blekinge-Rasmussen
- */
+/*******************************************************************************
+ * Copyright (c) 2007, 2010 The Planets Project Partners.
+ *
+ * All rights reserved. This program and the accompanying 
+ * materials are made available under the terms of the 
+ * Apache License, Version 2.0 which accompanies 
+ * this distribution, and is available at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package eu.planets_project.services.utils;
 
 import java.io.BufferedInputStream;
