b8372e4b9f7c571ca4f29b69c069a6ea0ca37ce1
Andrew Jackson <Andrew.Jackson@bl.uk>
2010-12-08 11:40:28 +0000

    Fixed up the tests, but required gitignore files to preserve the empty directories.

--- core/utils/src/test/java/eu/planets_project/services/utils/ZipUtilsTest.java
+++ core/utils/src/test/java/eu/planets_project/services/utils/ZipUtilsTest.java
@@ -29,8 +29,8 @@ import org.junit.Test;
  */
 public class ZipUtilsTest {
 	
-    private static final File PROJECT_BASE_FOLDER = new File(System.getProperty("app.dir"));
-	private static final File TEST_FILE_FOLDER = new File(PROJECT_BASE_FOLDER, "src/test/resources/test_zip");
+    private static final File PROJECT_BASE_FOLDER = new File(".");
+	private static final File TEST_FILE_FOLDER = new File(PROJECT_BASE_FOLDER, "src/test/data/test_zip");
 	
     private static File outputFolder = null;
 
@@ -110,10 +110,10 @@ public class ZipUtilsTest {
 		File extract = new File(outputFolder, folderName);
 		FileUtils.forceMkdir(extract);
 		ZipUtils.unzipTo(zip, extract);
-		new File(PROJECT_BASE_FOLDER,"src/test/resources/test_zip/images/test_jp2/canon-ixus.jpg.jp2");
+		new File(PROJECT_BASE_FOLDER,"src/test/data/test_zip/docs/lorem-ipsum.txt");
 //		File deleteSingleFile = new File("IF/common/src/test/resources/test_zip/images/test_jp2/canon-ixus.jpg.jp2");
-		ZipUtils.removeFileFrom(zip, "images/test_jp2/canon-ixus.jpg.jp2");
-		ZipUtils.removeFileFrom(zip, "images/test_gif");
+		ZipUtils.removeFileFrom(zip, "docs/lorem-ipsum.txt");
+		ZipUtils.removeFileFrom(zip, "docs");
 		System.out.println("Zip modified. Please find it here: " + zip.getAbsolutePath());
 	}
 
@@ -126,8 +126,8 @@ public class ZipUtilsTest {
 		FileUtils.cleanDirectory(outputFolder);
 		File zip = ZipUtils.createZip(TEST_FILE_FOLDER, outputFolder, "zipUtilsTestInsert.zip", true);
 		System.out.println("Zip created. Please find it here: " + zip.getAbsolutePath());
-		File toInsert = new File(PROJECT_BASE_FOLDER, "src/test/resources/test_zip/images/Kopie von test_gif");
-		ZipUtils.insertFileInto(zip, toInsert, "images/test_gif");
+		File toInsert = new File(PROJECT_BASE_FOLDER, "src/test/data/test_zip/docs");
+		ZipUtils.insertFileInto(zip, toInsert, "docs");
 //		File modifiedZip = ZipUtils.insertFileInto(zip, toInsert, "images/test_gif");
 //		File insertMore = new File("tests/test-files/documents/test_pdf/");
 //		modifiedZip = ZipUtils.insertFileInto(zip, insertMore, "documents/test_pdf");
@@ -145,7 +145,7 @@ public class ZipUtilsTest {
 		File zip = ZipUtils.createZip(TEST_FILE_FOLDER, outputFolder, "zipUtilsTestGetFile.zip", true);
 		System.out.println("Zip created. Please find it here: " + zip.getAbsolutePath());
 		
-		File fromZip = ZipUtils.getFileFrom(zip, "images/test_gif", outputFolder);
+		File fromZip = ZipUtils.getFileFrom(zip, "docs", outputFolder);
 		System.out.println("File extracted. Please find it here: " + fromZip.getAbsolutePath());
 	}
 
