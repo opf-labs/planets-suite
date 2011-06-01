ee9df293caa80860e71555e599e0a8c3faa9d6e3
Gerald de Jong <gerald@delving.eu>
2011-06-01 11:29:36 +0200

    DataStore and EntityManager bound to JNDI in PlatoStarter.  Problem: Transaction Manager

--- webapp-plato/src/test/java/eu/planets_project/pp/plato/run/PlatoStarter.java
+++ webapp-plato/src/test/java/eu/planets_project/pp/plato/run/PlatoStarter.java
@@ -1,13 +1,24 @@
 package eu.planets_project.pp.plato.run;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.log4j.Logger;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.security.HashUserRealm;
 import org.mortbay.jetty.security.UserRealm;
 import org.mortbay.jetty.webapp.WebAppContext;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Start up the web application from within the test context.  Good for debugging.
@@ -17,22 +28,52 @@ import java.io.IOException;
 
 public class PlatoStarter {
 
+    private static Logger LOG = Logger.getLogger(PlatoStarter.class);
+
     /**
      * @param args if not 8080 then pass port as an integer arg
      * @throws Exception when it goes wrong
      */
     public static void main(String... args) throws Exception {
-        System.setProperty("java.naming.factory.url.pkgs","org.mortbay.naming");
-        System.setProperty("java.naming.factory.initial","org.mortbay.naming.InitialContextFactory");
         int port = 8080;
         if (args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
+        LOG.info("Setting up JNDI");
+        System.setProperty("java.naming.factory.url.pkgs", "org.mortbay.naming");
+        System.setProperty("java.naming.factory.initial", "org.mortbay.naming.InitialContextFactory");
+        LOG.info("Binding data source");
+        bindDataSource();
+        LOG.info("Binding entity manager");
+        bindEntityManager();
+        LOG.info("Setting up Jetty server on port " + port);
         Server server = new Server(port);
         server.addHandler(new WebAppContext(getProjectRoot().getAbsolutePath() + "/webapp-plato/src/main/webapp", "/plato"));
         HashUserRealm planetsRealm = new HashUserRealm("PlanetsRealm");
-        server.setUserRealms(new UserRealm[] { planetsRealm });
+        server.setUserRealms(new UserRealm[]{planetsRealm});
         server.start();
+    }
+
+    private static void bindDataSource() throws PropertyVetoException, NamingException, SQLException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("org.hsqldb.jdbcDriver");
+        dataSource.setUser("sa");
+        dataSource.setPassword("");
+        dataSource.setJdbcUrl("jdbc:hsqldb:mem:testdb");
+        Connection con = dataSource.getConnection();
+        if (con != null) {
+            con.close();
+            new InitialContext().rebind("java:/platoDatasource", dataSource);
+        }
+        else {
+            throw new SQLException("Connection factory gives no connections!");
+        }
+    }
+
+    private static void bindEntityManager() throws NamingException {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("platoDatabase");
+        EntityManager entityManager = factory.createEntityManager();
+        new InitialContext().rebind("java:/platoEntityManagerFactory", entityManager);
     }
 
     private static File getProjectRoot() throws IOException {
